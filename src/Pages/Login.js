65033a8b0dbcc1d701f427233ce3e7f63e1b5334
José Demeneghi <josedemeneghi_cba@hotmail.com>
2021-09-21 15:30:54 -0400

    requisito 2

---
+++ src/Pages/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Login() {
+  return (
+    <div>
+      <input type="email" name="" id="" data-testid="email-input" />
+      <input type="password" name="" id="" data-testid="password-input" />
+      <button type="button" data-testid="login-submit-btn">Entrar</button>
+    </div>
+  );
+}
