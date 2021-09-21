718b249b940b9e9cd48d17defaa758f2c0870457
pedrodumorro <pa.coelho.castro@gmnail.com>
2021-09-21 17:32:26 -0300

    Req 19

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div
+      data-testid="footer"
+    >
+      <div
+        data-testid="drinks-bottom-btn"
+      />
+      <div
+        data-testid="explore-bottom-btn"
+      />
+      <div
+        data-testid="meal-bottom-btn"
+      />
+    </div>
+  );
+}
+
+export default Footer;
