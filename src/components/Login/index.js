b6f1004f99bfe5d843915ec6e7777051d938bc2b
fabio <fabiojuvenalpereira@hotmail.com>
2021-09-21 18:41:32 -0300

    req-02-03-04

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Login() {
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleChange({ target }) {
+    return target.name === 'user' ? setUser(target.value) : setPassword(target.value);
+  }
+
+  function handleClick() {
+    console.log('enviar para o estado global');
+  }
+
   return (
     <form>
-      <input />
+      <label htmlFor="user">
+        <input
+          type="text"
+          data-testid="email-input"
+          name="user"
+          value={ user }
+          onChange={ handleChange }
+        />
+      </label>
+      <label htmlFor="password">
+        <input
+          type="password"
+          data-testid="password-input"
+          name="password"
+          value={ password }
+          onChange={ handleChange }
+        />
+      </label>
+      <button
+        data-testid="login-submit-btn"
+        type="button"
+        onClick={ handleClick }
+      >
+        Entrar
+      </button>
     </form>
   );
 }
