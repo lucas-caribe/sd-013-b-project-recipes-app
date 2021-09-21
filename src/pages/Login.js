f8b02a1898f192a813b72a07807c8d6eba14f655
João Bruno Vasconcelos <joaobruno91@gmail.com>
2021-09-21 19:05:52 -0300

    [joaobruno05]- Adiciona o componente Footer

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Footer from '../components/Footer';
 
 function Login() {
   return (
     <div>
       <h1>Tela de Login</h1>
-      <Footer />
     </div>
   );
 }
