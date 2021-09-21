25243cd07549529d9fb715d4815e780b45edb3e4
José Demeneghi <josedemeneghi_cba@hotmail.com>
2021-09-21 18:40:15 -0400

    requisitos 72, 73 e 74

--- src/Pages/ExploreDrinks.js
+++ src/Pages/ExploreDrinks.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function ExploreDrinks() {
+export default function ExploreDrinks({ history }) {
   return (
     <div>
-      <button type="button" data-testid="explore-by-ingredient">Por Ingredientes</button>
-      <button type="button" data-testid="explore-surprise">Me Surpreenda!</button>
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+        onClick={ () => history.push('/explorar/bebidas/ingredientes') }
+      >
+        Por Ingredientes
+      </button>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        onClick={ () => history.push('/bebidas/178319') }
+      >
+        Me Surpreenda!
+      </button>
     </div>
   );
 }
+
+ExploreDrinks.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
--- src/Pages/ExploreFood.js
+++ src/Pages/ExploreFood.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export default function ExploreFood() {
+export default function ExploreFood({ history }) {
   return (
     <div>
-      <button type="button" data-testid="explore-by-ingredient">Por Ingredientes</button>
-      <button type="button" data-testid="explore-by-area">Por Local de Origem</button>
-      <button type="button" data-testid="explore-surprise">Me Surpreenda!</button>
+      <button
+        type="button"
+        data-testid="explore-by-ingredient"
+        onClick={ () => history.push('/explorar/comidas/ingredientes') }
+      >
+        Por Ingredientes
+      </button>
+      <button
+        type="button"
+        data-testid="explore-by-area"
+        onClick={ () => history.push('/explorar/comidas/area') }
+      >
+        Por Local de Origem
+      </button>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        onClick={ () => history.push('/comidas/52771') }
+      >
+        Me Surpreenda!
+      </button>
     </div>
   );
 }
+
+ExploreFood.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
