8191c97fda9aff526f129c35fe48bf2046cb714b
José Demeneghi <josedemeneghi_cba@hotmail.com>
2021-09-21 16:01:39 -0400

    requisitos 3, 4 e 5

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [buttonStatus, setButtonStatus] = useState(true);
+
+  useEffect(() => {
+    const emailValidInput = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    const MIN_LENGTH = 6;
+    if (email.match(emailValidInput) && password.length > MIN_LENGTH) {
+      setButtonStatus(false);
+    }
+  }, [email, password]);
+
   return (
     <div>
-      <input type="email" name="" id="" data-testid="email-input" />
-      <input type="password" name="" id="" data-testid="password-input" />
-      <button type="button" data-testid="login-submit-btn">Entrar</button>
+      <input
+        type="email"
+        data-testid="email-input"
+        onChange={ ({ target }) => setEmail(target.value) }
+      />
+      <input
+        type="password"
+        data-testid="password-input"
+        onChange={ ({ target }) => setPassword(target.value) }
+      />
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+        disabled={ buttonStatus }
+      >
+        Entrar
+      </button>
     </div>
   );
 }
