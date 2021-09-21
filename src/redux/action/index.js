fb37ae8ffe419577335b78d6e5ddd0511cda9277
Joao Victor <joaogmavictor@gmail.com>
2021-09-21 16:29:07 -0300

    Implementando Pastas Redux

---
+++ src/redux/action/index.js
@@ -0,0 +1,9 @@
+// Type
+
+export const SET_USER = 'SET_USER';
+
+// //Actions Creator
+
+export const setUser = (payload) => ({ type: SET_USER, payload });
+
+// // Thunk
---
+++ src/redux/reducer/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import loginData from './loginData';
+
+const rootReducer = combineReducers({
+  loginData,
+});
+
+export default rootReducer;
