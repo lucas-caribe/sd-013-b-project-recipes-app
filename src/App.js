4d83950808a5e2c6a8c01f7885741bac44c46d71
AC-Oliveira <allancardoso21@gmail.com>
2021-09-21 19:17:38 -0300

    Requisitos 9 ate 12 sem a 10

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import RecipesProvider from './Context/RecipesProvider';
 import Login from './Pages/Login';
 import MainFoodPage from './Pages/MainFoodPage';
+import PerfilPage from './Pages/PerfilPage';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={ Login } />
           <Route exact path="/comidas" component={ MainFoodPage } />
+          <Route exact path="/perfil" component={ PerfilPage } />
         </Switch>
       </BrowserRouter>
     </RecipesProvider>
