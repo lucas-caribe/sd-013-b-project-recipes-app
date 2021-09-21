67d675273d04b7ebb47e626cb6a20c2e3c5d903e
Julio Pereira <jandrade.pereira1@gmail.com>
2021-09-21 17:54:58 -0300

    Header - Insere a logica dos requisitos 9, 11 e 12

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Switch } from 'react-router';
+import FoodPage from './Pages/FoodPage';
+import DrinkPage from './Pages/DrinkPage';
+import Profile from './Pages/Profile';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <Switch>
+      <Route path="/perfil" component={ Profile } />
+      <Route path="/comidas" component={ FoodPage } />
+      <Route patch="/bebidas" component={ DrinkPage } />
+    </Switch>
   );
 }
 
---
+++ src/Pages/FoodPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from '../components/Header';
+
+export default function FoodPage() {
+  return (
+    <div>
+      <Header title="Comidas" />
+    </div>
+  );
+}
