0e86ef6ebe10c04ae928965d179d83fb97061b31
unknown <brunodevmoraes@gmail.com>
2021-09-21 18:12:57 -0300

    Setup Inicial

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Switch, Route } from 'react-router-dom';
 import Provider from './context/Provider';
+import Login from './components/Login';
 
 function App() {
   return (
     <Provider>
-      <div className="meals">
-        <span className="logo">TRYBE</span>
-        <object
-          className="rocksGlass"
-          type="image/svg+xml"
-          data={ rockGlass }
-        >
-          Glass
-        </object>
-      </div>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/comidas" component={ Foods } />
+        <Route path="/bebidas" component={ Drinks } />
+        <Route path="/comidas/:id-da-receita" component={ FoodRecipe } />
+        <Route path="/bebidas/:id-da-receita" component={ DrinkRecipe } />
+        <Route path="/comidas/:id-da-receita/in-progress" component={ FoodInProgress } />
+        <Route path="/bebidas/:id-da-receita/in-progress" component={ DrinkInProgress } />
+        <Route path="/explorar" component={ Explorer } />
+        <Route path="/explorar/comidas" component={ FoodExplorer } />
+        <Route path="/explorar/bebidas" component={ DrinkExplorer } />
+        <Route path="/explorar/comidas/ingredientes" component={ FoodIngredients } />
+        <Route path="/explorar/bebidas/ingredientes" component={ DrinkIngredients } />
+        <Route path="/explorar/comidas/area" component={ FoodArea } />
+        <Route path="/perfil" component={ Profile } />
+        <Route path="/receitas-feitas" component={ DoneRecipes } />
+        <Route path="/receitas-favoritas" component={ FavoriteRecipes } />
+      </Switch>
     </Provider>
   );
 }
