7cdfcd546f441e1628d7fe56c7a24fde9a18864c
pedrodumorro <pa.coelho.castro@gmnail.com>
2021-09-21 17:40:37 -0300

    Req 20

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
+import '../../App.css';
+import drinkIcon from '../../images/drinkIcon.svg';
+import exploreIcon from '../../images/exploreIcon.svg';
+import mealIcon from '../../images/mealIcon.svg';
 
 function Footer() {
   return (
     <div
+      className="footer"
       data-testid="footer"
     >
       <div
         data-testid="drinks-bottom-btn"
-      />
+      >
+        <img src={ drinkIcon } alt="drink" />
+      </div>
       <div
         data-testid="explore-bottom-btn"
-      />
+      >
+        <img src={ exploreIcon } alt="explore" />
+
+      </div>
       <div
         data-testid="meal-bottom-btn"
-      />
+      >
+        <img src={ mealIcon } alt="meal" />
+      </div>
     </div>
   );
 }
