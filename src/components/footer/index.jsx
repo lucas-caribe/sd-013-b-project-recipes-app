5817a789a57f0248093e1eb88dedf8f9ecdaf283
pedrodumorro <pa.coelho.castro@gmnail.com>
2021-09-21 18:38:27 -0300

    Compontentiza Footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,35 +1,40 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import '../../App.css';
 import drinkIcon from '../../images/drinkIcon.svg';
 import exploreIcon from '../../images/exploreIcon.svg';
 import mealIcon from '../../images/mealIcon.svg';
+import LinkItem from './LinkItem';
 
 function Footer() {
+  const drinkObj = {
+    testid: 'drinks-bottom-btn',
+    rota: '/bebidas',
+    sourceImg: drinkIcon,
+    singular: 'drink',
+  };
+
+  const exploreObj = {
+    testid: 'explore-bottom-btn',
+    rota: '/explorar',
+    sourceImg: exploreIcon,
+    singular: 'explorar',
+  };
+
+  const mealObj = {
+    testid: 'meals-bottom-btn',
+    rota: '/comidas',
+    sourceImg: mealIcon,
+    singular: 'comida',
+  };
+
   return (
     <div
       className="footer"
       data-testid="footer"
     >
-      <Link
-        to="/bebidas"
-        data-testid="drinks-bottom-btn"
-      >
-        <img src={ drinkIcon } alt="drink" />
-      </Link>
-      <Link
-        to="/explorar"
-        data-testid="explore-bottom-btn"
-      >
-        <img src={ exploreIcon } alt="explore" />
-
-      </Link>
-      <Link
-        to="/comidas"
-        data-testid="meal-bottom-btn"
-      >
-        <img src={ mealIcon } alt="meal" />
-      </Link>
+      <LinkItem objProps={ drinkObj } />
+      <LinkItem objProps={ exploreObj } />
+      <LinkItem objProps={ mealObj } />
     </div>
   );
 }
