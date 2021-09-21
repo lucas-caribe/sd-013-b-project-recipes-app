4a231ff35ffa45acd71944d9ff32edf0c280c385
Lucas T Franca <dev.lucasteixeira@gmail.com>
2021-09-21 18:46:24 -0300

    Requisito 2,3,4,5 Finalizados

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="wrapper">
-      Hello
+      <Switch>
+        <Route exact path="/" component={ Login } />
+      </Switch>
     </div>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,1 +1,57 @@
-// Tela de Login: requisitos 2 a 8;
+import React, { useState } from 'react';
+
+const INITIAL_STATE = {
+  email: '',
+  password: '',
+};
+
+let buttonStats = true;
+
+function Login() {
+  const [state, setState] = useState(INITIAL_STATE);
+  const { password, email } = state;
+
+  function handleChange({ target }) {
+    const { value, id } = target;
+    setState({
+      ...state,
+      [id]: value,
+    });
+  }
+
+  const emailValidator = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const emailValid = emailValidator.test(email);
+  const passwordValidator = 6;
+
+  if (emailValid && password.length > passwordValidator) {
+    buttonStats = false;
+  } else buttonStats = true;
+
+  return (
+    <div>
+      <input
+        id="email"
+        value={ email }
+        onChange={ handleChange }
+        type="email"
+        data-testid="email-input"
+      />
+      <input
+        id="password"
+        value={ password }
+        onChange={ handleChange }
+        type="password"
+        data-testid="password-input"
+      />
+      <button
+        disabled={ buttonStats }
+        type="button"
+        data-testid="login-submit-btn"
+      >
+        Entrar
+      </button>
+    </div>
+  );
+}
+
+export default Login;
