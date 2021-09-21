ab8095aaf7c122426437dae3627804aabb38f6e9
LuanAlexandre <luanrb19@gmail.com>
2021-09-21 19:52:43 -0400

    requisito 20

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import '../Styles/Footer.css';
+import drinkIcon from '../images/drinkIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
 
 export default function Footer() {
   return (
-    <footer data-testid="footer">
-      <button type="button" data-testid="drinks-bottom-btn">Bebidas</button>
-      <button type="button" data-testid="explore-bottom-btn">Explorar</button>
-      <button type="button" data-testid="food-bottom-btn">Comidas</button>
+    <footer
+      data-testid="footer"
+      className="footer"
+    >
+      <img
+        data-testid="drinks-bottom-btn"
+        src={ drinkIcon }
+        alt="botao de bebidas"
+      />
+      <img
+        data-testid="explore-bottom-btn"
+        src={ exploreIcon }
+        alt="botao de explorar"
+      />
+      <img
+        data-testid="food-bottom-btn"
+        src={ mealIcon }
+        alt="botao de comidas"
+      />
     </footer>
   );
 }
