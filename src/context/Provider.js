52934c780b902d3055cd686b2b624fab68ff17da
Daniel Teixeira <daniel.o.teixeira@gmail.com>
2021-09-21 17:14:30 -0300

    Requisitos 3, 4 e 5 concluidos

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Context from './Context';
 
 function Provider({ children }) {
+  const [statusLoginBtn, setStatusLoginBtn] = useState(true);
+  const [emailIsValid, setEmailIsValid] = useState(false);
+  const [passwordIsValid, setPasswordIsValid] = useState(false);
+
+  const contextValue = {
+    statusLoginBtn,
+    setStatusLoginBtn,
+    emailIsValid,
+    setEmailIsValid,
+    passwordIsValid,
+    setPasswordIsValid,
+  };
+
   return (
-    <Context.Provider>
+    <Context.Provider value={ contextValue }>
       {children}
     </Context.Provider>
   );
