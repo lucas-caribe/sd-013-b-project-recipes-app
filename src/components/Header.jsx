ec70a2e322300379e7e49d8be8ef8e5817c0850a
Julio Pereira <jandrade.pereira1@gmail.com>
2021-09-21 18:51:36 -0300

    Commit antes de mudar de branch

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
+import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 import SearchBar from './SearchBar';
