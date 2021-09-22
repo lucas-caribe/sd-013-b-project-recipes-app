427a52826d1c6e32c5c3ccdff993701a4885f13f
FilipeGuto <filipeguto95@gmail.com>
2021-09-22 18:35:56 -0300

    Requisito 85 e 86

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Button from '../components/Button';
 
 export default function Perfil() {
+  const history = useHistory();
+
+  function getReceitasFeitas() {
+    const redirect = '/receitas-feitas';
+    history.push(redirect);
+  }
+
+  function getReceitasFavoritas() {
+    const redirect = '/receitas-favoritas';
+    history.push(redirect);
+  }
+
   function getStorage() {
     const { email } = JSON.parse(localStorage.getItem('user'));
     return email;
@@ -13,10 +26,12 @@ export default function Perfil() {
       <Button
         text="Receitas Feitas"
         dataTest="profile-done-btn"
+        onClick={ getReceitasFeitas }
       />
       <Button
         text="Receitas Favoritas"
         dataTest="profile-favorite-btn"
+        onClick={ getReceitasFavoritas }
       />
       <Button
         text="Sair"
