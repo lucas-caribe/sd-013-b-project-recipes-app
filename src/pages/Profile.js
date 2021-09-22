23e2490efa60f16ac05439a34e02a7cd8eee3442
Rodrigo Cesar Destro Fidencio <60055837+rodrigocdfidencio@users.noreply.github.com>
2021-09-22 16:28:55 -0300

    Atualizando

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,14 +1,58 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Profile() {
+  const getEmail = () => {
+    if (localStorage.getItem('user')) {
+      const { email } = JSON.parse(localStorage.getItem('user'));
+      return email;
+    }
+  };
+
+  const history = useHistory();
+
+  const doneRecipes = () => {
+    history.push('/receitas-feitas');
+  };
+
+  const favoriteRecipes = () => {
+    history.push('/receitas-favoritas');
+  };
+
+  const loginPage = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
   return (
     <div className="recipes-page">
-      <Header />
+      <Header title="Perfil" />
+      <h1 data-testid="profile-email">{ getEmail() }</h1>
+      <button
+        className="profile-done-btn"
+        type="button"
+        onClick={ doneRecipes }
+      >
+        Receitas Feitas
+      </button>
+      <button
+        className="profile-favorite-btn"
+        type="button"
+        onClick={ favoriteRecipes }
+      >
+        Receitas Favoritas
+      </button>
+      <button
+        className="profile-logout-btn"
+        type="button"
+        onClick={ loginPage }
+      >
+        Sair
+      </button>
       <Footer />
     </div>
   );
 }
-
 export default Profile;
