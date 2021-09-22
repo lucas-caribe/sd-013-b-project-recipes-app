5f2a3ce8e9decda9d0e5e8f9faeb209d45447010
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2021-09-22 16:47:40 -0300

    :white_check_mark: Adicionando requisito 13
    Requisito 13 botões radio feitos

--- src/pages/Comidas.jsx
+++ src/pages/Comidas.jsx
@@ -5,6 +5,21 @@ export default function Comidas() {
   return (
     <div>
       <Header pageTitle="Comidas" />
+      <div>
+        <label htmlFor="ingredient">
+          Ingrediente
+          <input type="radio" name="ingredient" data-testid="ingredient-search-radio" />
+        </label>
+        <label htmlFor="name">
+          Nome
+          <input type="radio" name="name" data-testid="name-search-radio" />
+        </label>
+        <label htmlFor="ingredient">
+          Primeira letra
+          <input type="radio" name="ingredient" data-testid="first-letter-search-radio" />
+        </label>
+        <button type="button" data-testid="exec-search-btn">Buscar</button>
+      </div>
     </div>
   );
 }
