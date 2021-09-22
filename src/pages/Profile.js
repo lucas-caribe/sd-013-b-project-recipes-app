e3f078f9caa92e1a01ac7271c86d1b17a97cdcf5
Rodrigo Cesar Destro Fidencio <60055837+rodrigocdfidencio@users.noreply.github.com>
2021-09-22 16:31:49 -0300

    Criando página de Perfil do Usuário

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -4,7 +4,7 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Profile() {
-  const getEmail = () => {
+  const getEmail = () => { //  Chama o email do LocalStorage
     if (localStorage.getItem('user')) {
       const { email } = JSON.parse(localStorage.getItem('user'));
       return email;
