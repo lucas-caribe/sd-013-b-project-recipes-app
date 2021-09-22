4a5eee4b3197fc5b896f89a4979eb995b2e9200b
Gabriel <gribeiro237@gmail.com>
2021-09-22 16:31:17 -0300

    corrigindo gambiarra

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
 import store from './Redux/Store';
-import Login from './Pages/Login';
 
 function App() {
   return (
     <Provider store={ store }>
-      <Login />
+      <div>a</div>
     </Provider>
   );
 }
