06b151829452b0342075cabe5d2b4217e86529a2
FilipeGuto <filipeguto95@gmail.com>
2021-09-22 16:58:22 -0300

    Requisito 82

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import Button from '../components/Button';
 
 export default function Perfil() {
   return (
     <div>
-      <p>Perfil</p>
+      <h4 data-testid="profile-email">Email</h4>
+      <Button
+        text="Receitas Feitas"
+        dataTest="profile-done-btn"
+      />
+      <Button
+        text="Receitas Favoritadas"
+        dataTest="profile-favorite-btn"
+      />
+      <Button
+        text="Sair"
+        dataTest="profile-logout-btn"
+      />
     </div>
   );
 }
