ca6ed0717268468e12b54e8f5eb4bc5e6cf9fa87
Gabriel <gribeiro237@gmail.com>
2021-09-22 16:28:20 -0300

    requisitos 2 a 5

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
 import store from './Redux/Store';
+import Login from './Pages/Login';
 
 function App() {
   return (
     <Provider store={ store }>
-      <div>a</div>
+      <Login />
     </Provider>
   );
 }
---
+++ src/Pages/Login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import HeaderLogin from '../Components/HeaderLogin';
+
+class Login extends Component {
+  render() {
+    return (
+      <HeaderLogin />
+    );
+  }
+}
+
+export default Login;
