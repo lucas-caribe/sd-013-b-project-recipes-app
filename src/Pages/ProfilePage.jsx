23c7f305dd15d57052b3c736ab56832ef78aa2b3
Guilherme Moreno <guilherme.lmoreno23@gmail.com>
2021-09-22 18:14:25 -0300

    Requesitos 82-87

--- src/Pages/ProfilePage.jsx
+++ src/Pages/ProfilePage.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import HeaderWithoutSearch from './HeaderWithoutSearch';
 import Footer from '../Components/Footer';
 
 export default function ProfilePage() {
+  const currentEmail = JSON.parse(localStorage.getItem('user'));
   return (
     <>
       <HeaderWithoutSearch />
       <h3 data-testid="page-title">Perfil</h3>
+      <h3 data-testid="profile-email">{ currentEmail.email }</h3>
+      <Link to="/receitas-feitas">
+        <button type="button" data-testid="profile-done-btn">
+          Receitas Feitas
+        </button>
+      </Link>
+      <Link to="/receitas-favoritas">
+        <button type="button" data-testid="profile-favorite-btn">
+          Receitas Favoritas
+        </button>
+      </Link>
+      <Link to="/">
+        <button
+          type="button"
+          onClick={ () => { localStorage.clear(); } }
+          data-testid="profile-logout-btn"
+        >
+          Sair
+        </button>
+      </Link>
       <Footer />
     </>
   );
