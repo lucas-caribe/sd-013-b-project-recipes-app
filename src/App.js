e3cb3d2aa7b1e1c0269bb8ab31e83b90166f3a10
Mariana Garcia <maridgarcia045@gmail.com>
2021-09-22 17:11:33 -0300

    Corrige linha 6

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Provider from './context/Provider';
-import Login from './components/Login';
 import Explorar from './pages/Explorar';
+import Login from './components/Login';
 
 function App() {
   return (
