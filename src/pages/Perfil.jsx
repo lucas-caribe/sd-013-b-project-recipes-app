de07a17b6d5ed1f54547ca7af55c82167ede81ec
Marcus Vinicius Mendes do Nascimento <marcus.mendes89@gmail.com>
2021-09-22 00:58:42 -0300

    Requisito 82 Perfil e botoes

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,10 +1,34 @@
 // Tela de perfil: requisito 82 a 87;
 import React from 'react';
+import { useHistory } from 'react-router';
 
 function Perfil() {
+  const { history } = useHistory();
   return (
-    <div>
-      Perfil Page
+    <div className="wrapper">
+      {/* Requisito 82 */}
+      <p data-testid="profile-email">Email</p>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={ () => history.pushState('/') }
+      >
+        Receitas Feitas
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ () => history.pushState('/') }
+      >
+        Receitas Favoritas
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ () => history.pushState('/') }
+      >
+        Sair
+      </button>
     </div>
   );
 }
