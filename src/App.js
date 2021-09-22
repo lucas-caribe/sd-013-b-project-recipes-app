63291185a28d5fe4c3769fa5380684c75c0487b6
Felipe Ventorim <felipeventorim11@gmail.com>
2021-09-22 16:37:27 -0300

    [Grupo 26] Project Recipes App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import './App.css';
 import rockGlass from './images/rockGlass.svg';
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
