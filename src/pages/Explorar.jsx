3d8f5ac075079a1477112bfe359265e1d5b3eb49
iroDevs <ironegrolol@gamil.com>
2021-09-22 14:41:26 -0300

    Footer Component reque 19 ao 24

--- src/pages/Explorar.jsx
+++ src/pages/Explorar.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function Explorar() {
   return (
     <div>
       <Header pageTitle="Explorar" haveHeader={ false } />
       <div>Explorar</div>
+      <Footer />
     </div>
   );
 }
