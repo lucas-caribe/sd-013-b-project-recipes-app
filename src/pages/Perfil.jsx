13a9c8784600506ebc143dabf9050d994db5fe88
FilipeGuto <filipeguto95@gmail.com>
2021-09-22 17:57:01 -0300

    Requisito 83

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 import Button from '../components/Button';
 
 export default function Perfil() {
+  function getStorage() {
+    const { email } = JSON.parse(localStorage.getItem('user'));
+    console.log(email);
+    return email;
+  }
+
   return (
     <div>
-      <h4 data-testid="profile-email">Email</h4>
+      <h4 data-testid="profile-email">{ getStorage() }</h4>
       <Button
         text="Receitas Feitas"
         dataTest="profile-done-btn"
