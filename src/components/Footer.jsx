ee64fffdc7e07595348cb2cfa044d8f27ab78562
Letícia Castro <aicitelks@gmail.com>
2021-09-22 17:43:38 -0300

    Implementação básica: <Footer /> <Comidas /> e rota da página principal comidas no App

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Footer() {
+  return (
+    <div>
+      <footer data-testid="footer">
+        Footer
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/pages/Comidas.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Footer from '../components/Footer';
+
+function Comidas() {
+  return (
+    <div>
+      <h3>Tela principal de receitas de comidas</h3>
+      <Footer />
+    </div>
+  );
+}
+
+export default Comidas;
