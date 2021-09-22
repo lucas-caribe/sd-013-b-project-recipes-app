427bb1cb1ad4c9195765210d1aaa986aba300108
Marcus Vinicius Mendes do Nascimento <marcus.mendes89@gmail.com>
2021-09-22 12:50:12 -0300

    Requisitos 67, 68, 69 explorar

--- src/pages/Explorer.jsx
+++ src/pages/Explorer.jsx
@@ -1,10 +1,25 @@
 // Tela de explorar: Requisitos 67 a 69
 import React from 'react';
+import { useHistory } from 'react-router';
 
 function Explorer() {
+  const history = useHistory();
   return (
     <div>
-      Explorer Page
+      <button
+        type="button"
+        data-testid="explore-food"
+        onClick={ () => history.push('/explorar/comidas') }
+      >
+        Explorar Comidas
+      </button>
+      <button
+        type="button"
+        data-testid="explore-drinks"
+        onClick={ () => history.push('/explorar/bebidas') }
+      >
+        Explorar Bebidas
+      </button>
     </div>
   );
 }
