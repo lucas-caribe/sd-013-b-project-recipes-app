cb5e9b742cd2d6a41f72a82936ad1c0d9c3d9911
João Bruno Vasconcelos <joaobruno91@gmail.com>
2021-09-22 17:44:56 -0300

    [joaobruno05]- Tela Explore

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
@@ -7,6 +8,25 @@ function Explore() {
   return (
     <div className="explore-page">
       <Header />
+
+      <Link to="/explorar/comidas">
+        <button
+          type="button"
+          data-testid="explore-food"
+        >
+          Explorar Comidas
+        </button>
+      </Link>
+
+      <Link to="/explorar/bebidas">
+        <button
+          type="button"
+          data-testid="explore-drinks"
+        >
+          Explorar Bebidas
+        </button>
+      </Link>
+
       <Footer />
     </div>
   );
