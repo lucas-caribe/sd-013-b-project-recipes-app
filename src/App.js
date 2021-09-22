15a91358f6bf8750c7f461fd3490bfecbbf86653
Guilherme <guilhermerg1234@gmail.com>
2021-09-22 14:01:45 -0300

    10 quase pronta e 11 pronta

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Comidas from './pages/Comidas';
+import Bebidas from './pages/Bebidas';
+import Explorar from './pages/Explorar';
+import ExplorarComidas from './pages/ExplorarComidas';
+import ExplorarBebidas from './pages/ExplorarBebidas';
+import ExplorarComidasIngredientes from './pages/ExplorarComidasIngredientes';
+import ExplorarBebidasIngredientes from './pages/ExplorarBebidasIngredientes';
+import ExplorarOrigem from './pages/ExplorarOrigem';
+import Perfil from './pages/Perfil';
+import ReceitasFeitas from './pages/ReceitasFeitas';
+import ReceitasFavoritas from './pages/ReceitasFavoritas';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import Header from './components/header';
 
@@ -9,6 +20,17 @@ function App() {
     <div className="App">
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route exact path="/comidas" component={ Comidas } />
+        <Route exact path="/bebidas" component={ Bebidas } />
+        <Route exact path="/explorar" component={ Explorar } />
+        <Route exact path="/explorar/comidas" component={ ExplorarComidas } />
+        <Route exact path="/explorar/bebidas" component={ ExplorarBebidas } />
+        <Route exact path="/explorar/comidas/ingredientes" component={ ExplorarComidasIngredientes } />
+        <Route exact path="/explorar/bebidas/ingredientes" component={ ExplorarBebidasIngredientes } />
+        <Route exact path="/explorar/comidas/area" component={ ExplorarOrigem } />
+        <Route exact path="/perfil" component={ Perfil } />
+        <Route exact path="/receitas-feitas" component={ ReceitasFeitas } />
+        <Route exact path="/receitas-favoritas" component={ ReceitasFavoritas } />
       </Switch>
     </div>
   );
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,19 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import profileIcon from '../../images/profileIcon.svg';
+import searchIcon from '../../images/searchIcon.svg';
 
-function Header() {
+function Header(props) {
+  const { titlePage } = props;
   return (
     <div>
       <header>
-        <h1 data-testid="page-title">Comidas</h1>
-        <button data-testid="profile-top-btn">
-          <img src="src/images/profileIcon.svg" alt="userIcon" />
-        </button>
-        <button data-testid="search-top-btn">
-          <img src="src/images/searchIcon.svg" alt="searchIcon" />
-        </button>
+        <h1 data-testid="page-title">{ titlePage }</h1>
+        <Link to="/perfil">
+          <img
+            src={ profileIcon }
+            alt="userIcon"
+            data-testid="profile-top-btn"
+          />
+        </Link>
+        {!titlePage.includes('Explorar') && <img
+          src={ searchIcon }
+          alt="searchIcon"
+          data-testid="search-top-btn"
+        />}
       </header>
     </div>
   );
 }
+
+Header.propTypes = {
+  titlePage: PropTypes.string.isRequired,
+};
 
 export default Header;
