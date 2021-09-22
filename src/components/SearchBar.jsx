8745553753e55a760d6e4545a2054e4515568bf0
Alessandro Hugen <alessandrohugen@gmail.com>
2021-09-22 18:25:58 -0300

    requisito 14

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,28 +3,56 @@ import { ingredientAPI, nameAPI, fistLetterAPI } from '../services/foodAPI';
 
 export default function SearchBar() {
   const [text, setText] = useState('');
-
-  async function handleIngredient(ingredient) {
-    const ingredientSearch = await ingredientAPI(ingredient);
-    console.log(ingredientSearch);
-  }
-
-  async function handleName(name) {
-    const nameSearch = await nameAPI(name);
-    console.log(nameSearch);
-  }
-
-  async function handleLetter(letter) {
-    const letterSearch = await fistLetterAPI(letter);
-    console.log(letterSearch);
-  }
+  const [ingredient, setIngredient] = useState(false);
+  const [name, setName] = useState(false);
+  const [letter, setLetter] = useState(false);
 
   const checkLetter = (input) => {
     if (input.length !== 1) {
       global.alert('Sua busca deve conter somente 1 (um) caracter');
     }
-    handleLetter(input);
+    setLetter(input);
   };
+
+  function handleIngredient() {
+    setIngredient(!ingredient);
+    setName(false);
+    setLetter(false);
+  }
+
+  function handleName() {
+    setName(!name);
+    setIngredient(false);
+    setLetter(false);
+  }
+
+  function handleLetter(l) {
+    checkLetter(l);
+    setLetter(!letter);
+    setIngredient(false);
+    setName(false);
+  }
+
+  async function iAPI(i) {
+    const ingredientSearch = await ingredientAPI(i);
+    console.log(ingredientSearch);
+  }
+
+  async function nAPI(n) {
+    const nameSearch = await nameAPI(n);
+    console.log(nameSearch);
+  }
+
+  async function lAPI(l) {
+    const letterSearch = await fistLetterAPI(l);
+    console.log(letterSearch);
+  }
+
+  function handleClick() {
+    if (ingredient) iAPI(text);
+    if (name) nAPI(text);
+    if (letter) lAPI(text);
+  }
 
   return (
     <div>
@@ -32,7 +60,7 @@ export default function SearchBar() {
         type="text"
         data-testid="search-input"
         placeholder="Pesquise sua receita"
-        onChange={ (e) => (setText(e.target.value)) }
+        onChange={ (e) => setText(e.target.value) }
       />
       <label htmlFor="ingredient-search">
         Ingrediente
@@ -41,7 +69,7 @@ export default function SearchBar() {
           data-testid="ingredient-search-radio"
           id="ingredient-search"
           name="recepies"
-          onClick={ () => (handleIngredient(text)) }
+          onClick={ () => (handleIngredient()) }
         />
       </label>
       <label htmlFor="name-search">
@@ -51,7 +79,7 @@ export default function SearchBar() {
           data-testid="name-search-radio"
           id="name-search"
           name="recepies"
-          onClick={ () => (handleName(text)) }
+          onClick={ () => (handleName()) }
         />
       </label>
       <label htmlFor="first-letter-search">
@@ -61,12 +89,13 @@ export default function SearchBar() {
           data-testid="first-letter-search-radio"
           id="first-letter-search"
           name="recepies"
-          onClick={ () => (checkLetter(text)) }
+          onClick={ () => (handleLetter(text)) }
         />
       </label>
       <button
         type="button"
         data-testid="exec-search-btn"
+        onClick={ handleClick }
       >
         Buscar
       </button>
