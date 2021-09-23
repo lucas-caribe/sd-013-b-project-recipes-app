df87d65dd4c2f7f26cc5d0d6ec93bf883d09cee1
Matheus Figueiredo <mathhsoares2@live.com>
2021-09-23 16:21:42 -0300

    Fix lint errors

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import Header from '../components/Header';
+import Header from '../Components/Header';
 
 const Main = () => {
   const { id, type, status } = useParams();
