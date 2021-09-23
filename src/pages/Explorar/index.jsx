020646107df46f586c4d19bb77f7e79e7dd11af5
Rafael Nery Machado <rafaelnerymachado@gmail.com>
2021-09-23 15:21:53 -0300

    requisito 67 a 69 - pagina Explorar - concluido

--- src/pages/Explorar/index.jsx
+++ src/pages/Explorar/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
@@ -7,6 +8,10 @@ function Explorar() {
   return (
     <main>
       <Header pageTitle="Explorar" showSearchIcon={ false } />
+      <main className="explorer-container">
+        <Link to="/explorar/comidas" data-testid="explore-food">Explorar Comidas</Link>
+        <Link to="/explorar/bebidas" data-testid="explore-drinks">Explorar Bebidas</Link>
+      </main>
       <Footer />
     </main>
   );
