8692f93663e5b1d7c451fae838e125742a4ee77c
FilipeGuto <filipeguto95@gmail.com>
2021-09-23 17:58:24 -0300

    Requisitos 67 ao 69

--- src/pages/Explorar.jsx
+++ src/pages/Explorar.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import Button from '../components/Button';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 export default function Explorar() {
+  const history = useHistory();
+
+  function getFood() {
+    const redirect = '/explorar/comidas';
+    history.push(redirect);
+  }
+
+  function getDrinks() {
+    const redirect = '/explorar/bebidas';
+    history.push(redirect);
+  }
   return (
     <main className="main-content">
       <Header pageTitle="Explorar" searchButton={ false } />
+      <Button
+        text="Explorar Comidas"
+        dataTest="explore-food"
+        onClick={ getFood }
+      />
+      <Button
+        text="Explorar Bebidas"
+        dataTest="explore-drinks"
+        onClick={ getDrinks }
+      />
       <Footer />
     </main>
   );
