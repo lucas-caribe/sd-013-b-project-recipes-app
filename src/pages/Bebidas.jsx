ccf9572081ee2732a8db47284cff4c0214e29f9a
Guilherme <guilhermerg1234@gmail.com>
2021-09-23 16:45:22 -0300

    12

--- src/pages/Bebidas.jsx
+++ src/pages/Bebidas.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import Header from '../components/header';
-import SearchBar from '../components/searchBar';
 import CocktailList from '../components/cocktails/CocktailList';
 
 export default function Bebidas() {
   return (
     <div>
       <Header titlePage="Bebidas" hasSearchIcon />
-      <SearchBar recipe="cocktail" />
       <CocktailList />
     </div>
   );
