975f6534f411624f7de34136988a1487560733b1
ricand7 <ricantonioj@hotmail.com>
2021-09-23 20:06:35 -0300

    versao 0 req 1 a 8

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './Login/Login';
 
 function App() {
   return (
     <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
+      <span className="logo">App de Receitas</span>
+      <div>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          {/* <Route exact path="/comidas" component={ comidas } /> */}
+        </Switch>
+      </div>
     </div>
   );
 }
---
+++ src/Login/Login.js
@@ -0,0 +1,101 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+function Login() {
+  const history = useHistory();
+  const [email, setUserEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // const mealisAuthenticated = () => localStorage.getItem(mealsToken) !== null;
+  // const cockisAuthenticated = () => localStorage.getItem(cocktailsToken) !== null;
+  // const getMealsToken = () => localStorage.getItem(mealsToken);
+  // const getCockToken = () => localStorage.getItem(cocktailsToken);
+
+  const loginmeals = (token) => {
+    localStorage.setItem('mealsToken', token);
+  };
+  const loginCock = (token) => {
+    localStorage.setItem('cocktailsToken', token);
+  };
+  const emailStorage = (usemail) => {
+    localStorage.setItem('user', usemail);
+  };
+  // const logout = () => {
+  //   localStorage.removeItem(meals, cocktail);
+  // };
+
+  const handleChange = ({ target }) => {
+    setUserEmail(target.value);
+  };
+
+  const handlePassword = ({ target }) => {
+    setPassword(target.value);
+  };
+
+  const redirectToTarget = () => {
+    history.push('/comidas');
+    loginmeals(1);
+    loginCock(1);
+    emailStorage(email);
+  };
+
+  const validaLogin = () => {
+    const passwordLength = 6;
+    let loginValidado = true;
+
+    if (email.includes('@' && '.com') && password.length >= passwordLength) {
+      loginValidado = false;
+    } else {
+      loginValidado = true;
+    }
+    return loginValidado;
+  };
+
+  return (
+    <form>
+      <label htmlFor="email">
+        <input
+          className="em-input"
+          type="email"
+          data-testid="email-input"
+          name="email"
+          onChange={ handleChange }
+          value={ email }
+        />
+      </label>
+
+      <label htmlFor="password">
+        <input
+          className="em-input"
+          type="text"
+          data-testid="password-input"
+          name="password"
+          onChange={ handlePassword }
+          value={ password }
+        />
+      </label>
+
+      <button
+        type="submit"
+        onClick={ redirectToTarget }
+        disabled={ validaLogin() }
+      >
+        Entrar
+      </button>
+    </form>
+  );
+}
+
+Login.propTypes = {
+  // history: PropTypes.objectOf(PropTypes.any).isRequired,
+  // userEmail: PropTypes.func.isRequired,
+};
+
+// const mapDispatchToProps = (dispatch) => ({
+//   // userEmail: (email) => dispatch(userAction(email)),
+
+// });
+
+export default Login;
