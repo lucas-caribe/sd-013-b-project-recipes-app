a07bb234f7278c9516fe17975fec5d392f9436d6
alanmdf <alanmoreirascr@gmail.com>
2021-09-23 17:29:51 -0300

    Requisito 34 finalizado

--- src/pages/Detalhes/index.jsx
+++ src/pages/Detalhes/index.jsx
@@ -1,28 +1,121 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 
-function Detalhes() {
-  const id = 52900;
-  const [hasVideo, setHasVideo] = useState(true);
-  return (
-    <main>
-      <img data-testid="recipe-photo" src="https://www.themealdb.com/images/media/meals/urtqut1511723591.jpg" alt="" height="400px" width="400px" />
-      <h1 data-testid="recipe-title">Xablau</h1>
-      <button data-testid="share-btn" type="button">Share</button>
-      <button data-testid="favorite-btn" type="button">Favorite</button>
-      <h2 data-testid="recipe-category">Xableu</h2>
-      <ul>
-        <li data-testid="0-ingredient-name-and-measure">Ingrediente 1</li>
-        <li data-testid="1-ingredient-name-and-measure">Ingrediente 2</li>
-      </ul>
-      <p data-testid="instructions">Instruções</p>
-      {hasVideo && <iframe data-testid="video" src="https://www.youtube.com/watch?v=-YDh4WEmK_E" title="recipe-video" frameBorder="0" /> }
-      <div>
-        <div data-testid="0-recomendation-card">Card 1</div>
-        <div data-testid="1-recomendation-card">Card 2</div>
-      </div>
-      <button data-testid="start-recipe-btn" type="button">Iniciar receita</button>
-    </main>
-  );
+function Detalhes({ location: { pathname } }) {
+  const [details, setDetails] = useState({});
+
+  useEffect(() => {
+    const fetchRecipe = async () => {
+      if (pathname.includes('comidas')) {
+        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${pathname.slice(9)}`);
+        const result = await response.json();
+        setDetails(result.meals[0]);
+      } else {
+        const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${pathname.slice(9)}`);
+        const result = await response.json();
+        setDetails(result.drinks[0]);
+      }
+    };
+
+    fetchRecipe();
+  }, [pathname]);
+
+  const mealsDetails = () => {
+    if (Object.values(details).length === 0) {
+      return <span>Carregando...</span>;
+    } return (
+      <main>
+        <img
+          data-testid="recipe-photo"
+          src={ details.strMealThumb }
+          alt={ details.strMeal }
+          height="400px"
+          width="400px"
+        />
+        <h1 data-testid="recipe-title">{ details.strMeal }</h1>
+        <button data-testid="share-btn" type="button">Share</button>
+        <button data-testid="favorite-btn" type="button">Favorite</button>
+        <h2 data-testid="recipe-category">{ details.strCategory }</h2>
+        <ul>
+          {Object.entries(details)
+            .filter((ingredient) => ingredient[0].includes('Ingredient')
+            && ingredient[1])
+            .map((filteredIngredient, index) => (
+              <li
+                key={ index }
+                data-testid={ `${index}-ingredient-name-and-measure` }
+              >
+                {filteredIngredient[1]}
+              </li>
+            ))}
+        </ul>
+        <p data-testid="instructions">{details.strInstructions}</p>
+        {details.strYoutube
+      && <iframe
+        data-testid="video"
+        src={ details.strYoutube }
+        title={ details.strMeal }
+        frameBorder="0"
+      /> }
+        <div>
+          <div data-testid="0-recomendation-card">Card 1</div>
+          <div data-testid="1-recomendation-card">Card 2</div>
+        </div>
+        <button data-testid="start-recipe-btn" type="button">Iniciar receita</button>
+      </main>
+    );
+  };
+
+  const drinkDetails = () => {
+    if (Object.values(details).length === 0) {
+      return <span>Carregando...</span>;
+    } return (
+      <main>
+        <img
+          data-testid="recipe-photo"
+          src={ details.strDrinkThumb }
+          alt={ details.strDrink }
+          height="400px"
+          width="400px"
+        />
+        <h1 data-testid="recipe-title">{ details.strDrink }</h1>
+        <button data-testid="share-btn" type="button">Share</button>
+        <button data-testid="favorite-btn" type="button">Favorite</button>
+        <h2 data-testid="recipe-category">{ details.strCategory }</h2>
+        <ul>
+          {Object.entries(details)
+            .filter((ingredient) => ingredient[0].includes('Ingredient')
+            && ingredient[1])
+            .map((newIngredient, index) => (
+              <li
+                key={ index }
+                data-testid={ `${index}-ingredient-name-and-measure` }
+              >
+                {newIngredient[1]}
+              </li>
+            ))}
+        </ul>
+        <p data-testid="instructions">{details.strInstructions}</p>
+        {details.strVideo
+      && <iframe
+        data-testid="video"
+        src={ details.strVideo }
+        title={ details.strDrink }
+        frameBorder="0"
+      /> }
+        <div>
+          <div data-testid="0-recomendation-card">Card 1</div>
+          <div data-testid="1-recomendation-card">Card 2</div>
+        </div>
+        <button data-testid="start-recipe-btn" type="button">Iniciar receita</button>
+      </main>
+    );
+  };
+
+  if (pathname.includes('comidas')) {
+    return mealsDetails();
+  }
+  return drinkDetails();
 }
 
 export default Detalhes;
