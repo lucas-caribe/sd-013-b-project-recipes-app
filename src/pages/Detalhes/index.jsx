e3b17391ef6e2a76371fe65e638e630da07bdfdb
alanmdf <alanmoreirascr@gmail.com>
2021-09-23 19:54:59 -0300

    Requisito 35 finalizado

--- src/pages/Detalhes/index.jsx
+++ src/pages/Detalhes/index.jsx
@@ -1,62 +1,51 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { useDetails } from '../../context/DetailsContext';
 
 function Detalhes({ location: { pathname } }) {
-  const [details, setDetails] = useState({});
+  const { fetchRecipe, item, ingredients } = useDetails();
 
   useEffect(() => {
-    const fetchRecipe = async () => {
-      if (pathname.includes('comidas')) {
-        const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${pathname.slice(9)}`);
-        const result = await response.json();
-        setDetails(result.meals[0]);
-      } else {
-        const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${pathname.slice(9)}`);
-        const result = await response.json();
-        setDetails(result.drinks[0]);
-      }
-    };
-
-    fetchRecipe();
+    fetchRecipe(pathname);
   }, [pathname]);
 
-  const mealsDetails = () => {
-    if (Object.values(details).length === 0) {
+  const renderIngredients = () => ingredients
+    .map((ingredient, index) => (
+      <li
+        key={ index }
+        data-testid={ `${index}-ingredient-name-and-measure` }
+      >
+        {ingredient}
+      </li>
+    ));
+
+  const mealDetails = () => {
+    if (!item.meal) {
       return <span>Carregando...</span>;
     } return (
       <main>
         <img
           data-testid="recipe-photo"
-          src={ details.strMealThumb }
-          alt={ details.strMeal }
+          src={ item.meal[0].strMealThumb }
+          alt={ item.meal[0].strMeal }
           height="400px"
           width="400px"
         />
-        <h1 data-testid="recipe-title">{ details.strMeal }</h1>
+        <h1 data-testid="recipe-title">{ item.meal[0].strMeal }</h1>
         <button data-testid="share-btn" type="button">Share</button>
         <button data-testid="favorite-btn" type="button">Favorite</button>
-        <h2 data-testid="recipe-category">{ details.strCategory }</h2>
+        <h2 data-testid="recipe-category">{ item.meal[0].strCategory }</h2>
         <ul>
-          {Object.entries(details)
-            .filter((ingredient) => ingredient[0].includes('Ingredient')
-            && ingredient[1])
-            .map((filteredIngredient, index) => (
-              <li
-                key={ index }
-                data-testid={ `${index}-ingredient-name-and-measure` }
-              >
-                {filteredIngredient[1]}
-              </li>
-            ))}
+          {renderIngredients()}
         </ul>
-        <p data-testid="instructions">{details.strInstructions}</p>
-        {details.strYoutube
-      && <iframe
-        data-testid="video"
-        src={ details.strYoutube }
-        title={ details.strMeal }
-        frameBorder="0"
-      /> }
+        <p data-testid="instructions">{item.meal[0].strInstructions}</p>
+        {item.meal[0].strYoutube
+        && <iframe
+          data-testid="video"
+          src={ item.meal[0].strYoutube }
+          title={ item.meal[0].strMeal }
+          frameBorder="0"
+        /> }
         <div>
           <div data-testid="0-recomendation-card">Card 1</div>
           <div data-testid="1-recomendation-card">Card 2</div>
@@ -67,42 +56,25 @@ function Detalhes({ location: { pathname } }) {
   };
 
   const drinkDetails = () => {
-    if (Object.values(details).length === 0) {
+    if (!item.drink) {
       return <span>Carregando...</span>;
     } return (
       <main>
         <img
           data-testid="recipe-photo"
-          src={ details.strDrinkThumb }
-          alt={ details.strDrink }
+          src={ item.drink[0].strDrinkThumb }
+          alt={ item.drink[0].strDrink }
           height="400px"
           width="400px"
         />
-        <h1 data-testid="recipe-title">{ details.strDrink }</h1>
+        <h1 data-testid="recipe-title">{ item.drink[0].strDrink }</h1>
         <button data-testid="share-btn" type="button">Share</button>
         <button data-testid="favorite-btn" type="button">Favorite</button>
-        <h2 data-testid="recipe-category">{ details.strCategory }</h2>
+        <h2 data-testid="recipe-category">{ item.drink[0].strAlcoholic}</h2>
         <ul>
-          {Object.entries(details)
-            .filter((ingredient) => ingredient[0].includes('Ingredient')
-            && ingredient[1])
-            .map((newIngredient, index) => (
-              <li
-                key={ index }
-                data-testid={ `${index}-ingredient-name-and-measure` }
-              >
-                {newIngredient[1]}
-              </li>
-            ))}
+          {renderIngredients()}
         </ul>
-        <p data-testid="instructions">{details.strInstructions}</p>
-        {details.strVideo
-      && <iframe
-        data-testid="video"
-        src={ details.strVideo }
-        title={ details.strDrink }
-        frameBorder="0"
-      /> }
+        <p data-testid="instructions">{item.drink[0].strInstructions}</p>
         <div>
           <div data-testid="0-recomendation-card">Card 1</div>
           <div data-testid="1-recomendation-card">Card 2</div>
@@ -113,7 +85,7 @@ function Detalhes({ location: { pathname } }) {
   };
 
   if (pathname.includes('comidas')) {
-    return mealsDetails();
+    return mealDetails();
   }
   return drinkDetails();
 }
