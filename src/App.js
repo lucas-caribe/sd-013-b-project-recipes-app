abcc3045d70a23b36f3942bf807fac7862cc5cfa
Bernardo Raffide <bernardoraffide@pop-os.localdomain>
2021-09-23 10:36:04 -0300

    testes da pagina de login

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import LoginScreen from './pages/login/LoginScreen';
+import LoginScreen from './pages/LoginScreen';
 
 function App() {
   return (
     <Switch>
       <Route path="/" component={ LoginScreen } />
+
     </Switch>
   );
 }
