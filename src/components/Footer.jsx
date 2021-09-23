1d4b46ea15cce72c7873fdffb2793f6713ce2f78
Letícia Castro <aicitelks@gmail.com>
2021-09-22 21:40:49 -0300

    :ballot_box_with_check: Requisito 20, teste ok

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,38 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import drinkIcon from '../images/drinkIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
+import exploreIcon from '../images/exploreIcon.svg';
 
 function Footer() {
   return (
-    <div>
-      <footer data-testid="footer">
-        <Link to="/" data-testid="drinks-bottom-btn">Drinks</Link>
-        <Link to="/" data-testid="explore-bottom-btn">Explorar</Link>
-        <Link to="/" data-testid="food-bottom-btn">Comidas</Link>
-      </footer>
-    </div>
+    <footer
+      data-testid="footer"
+      className="fixed-bottom"
+    >
+      <a
+        href="/"
+        src={ drinkIcon }
+        data-testid="drinks-bottom-btn"
+      >
+        <img src={ drinkIcon } alt="Drinks" />
+      </a>
+
+      <a
+        href="/"
+        src={ exploreIcon }
+        data-testid="explore-bottom-btn"
+      >
+        <img src={ exploreIcon } alt="Explore" />
+      </a>
+
+      <a
+        href="/"
+        src={ mealIcon }
+        data-testid="food-bottom-btn"
+      >
+        <img src={ mealIcon } alt="Food" />
+      </a>
+    </footer>
   );
 }
 
