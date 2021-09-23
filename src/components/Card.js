55bef76aae527703ab04a8d8601ae6141ad66c19
Rodrigo Cesar Destro Fidencio <60055837+rodrigocdfidencio@users.noreply.github.com>
2021-09-23 09:47:09 -0300

    Alteração no Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,64 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Card(name, image, id) {
-  return (
-    <div data-testid={ `${id}-recipe-card` }>
-      <img data-testid={ `${id}-card-img` } src={ image } alt="ab" />
-      <div data-testid={ `${id}-card-name` }>
-        {name}
+const Card = ({ type, index, thumb, name }) => {
+  if (type === 'ingredient') {
+    return (
+      <div
+        className="card"
+        id={ `${type.toLowerCase()}-card-${index}` }
+        data-testid={ `${index}-ingredient-card` }
+      >
+        <img
+          className="thumb"
+          id={ `${type.toLowerCase()}-thumb-${index}` }
+          data-testid={ `${index}-card-img` }
+          src={ thumb }
+          alt={ `Imagem ilustrativa de ${name}` }
+        />
+        <span
+          className="name"
+          id={ `${type.toLowerCase()}-name-${index}` }
+          data-testid={ `${index}-card-name` }
+        >
+          { name }
+        </span>
       </div>
+    );
+  }
+  return (
+    <div
+      className="card"
+      id={ `${type.toLowerCase()}-card-${index}` }
+      data-testid={ `${index}-recipe-card` }
+    >
+      <img
+        className="thumb"
+        id={ `${type.toLowerCase()}-thumb-${index}` }
+        data-testid={ `${index}-card-img` }
+        src={ thumb }
+        alt={ `Imagem ilustritiva de ${name}` }
+      />
+      <span
+        className="name"
+        id={ `${type.toLowerCase()}-name-${index}` }
+        data-testid={ `${index}-card-name` }
+      >
+        { name }
+      </span>
     </div>
   );
-}
+};
+Card.propTypes = {
+  type: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
+  thumb: PropTypes.string,
+  name: PropTypes.string,
+};
+
+Card.defaultProps = {
+  thumb: undefined,
+  name: undefined,
+};
 
 export default Card;
