8262511e2fae9179234cbde200456a443052a72d
Marcus Vinicius Mendes do Nascimento <marcus.mendes89@gmail.com>
2021-09-23 16:42:19 -0300

    Services

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,54 @@
 // Tela principal de receitas: requisitos 25 a 32;
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { useHistory } from 'react-router';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import SearchBar from '../components/SearchBar';
+import { foodRequest, drinkRequest } from '../services/data';
+import CardList from '../components/CardList';
 
 function Home({ search }) {
-  console.log(search);
+  const [foods, setFoods] = useState([]);
+  const [drinks, setDrinks] = useState([]);
+  const { location: { pathname } } = useHistory();
+
+  useEffect(() => {
+    async function setFetch() {
+      if (pathname === '/comidas') {
+        setFoods(await foodRequest('search.php?s'));
+      }
+
+      if (pathname === '/bebidas') {
+        setDrinks(await drinkRequest('search.php?s'));
+      }
+    }
+    setFetch();
+  }, [pathname]);
+
+  // console.log(drinks);
+
+  if (foods === [] || drinks === []) {
+    return (
+      <div>
+        <p> Loading </p>
+      </div>
+    );
+  }
+
   return (
     <div>
-      <Header setTitle="Comidas" />
+      {console.log(foods)}
+      {
+        (pathname === '/comidas')
+          ? <Header setTitle="Comidas" /> : <Header setTitle="Bebidas" />
+      }
+
+      {(foods.length > 0) ? <CardList foods={ foods } /> : <CardList drinks={ drinks } />}
+      {/* <Header setTitle="Comidas" /> */}
       {search === true ? <SearchBar /> : null}
+
       <Footer />
     </div>
   );
