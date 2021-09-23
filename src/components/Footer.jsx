d92a0674040b5c698a50974dc87b3e3c4254964f
Letícia Castro <aicitelks@gmail.com>
2021-09-23 16:55:01 -0300

    <Footer /> Adicionado as páginas existentes: comidas, bebidas, perfil.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,31 +9,52 @@ function Footer() {
       data-testid="footer"
       className="fixed-bottom"
     >
-      <a
-        href="/"
-        src={ drinkIcon }
-        data-testid="drinks-bottom-btn"
-      >
-        <img src={ drinkIcon } alt="Drinks" />
-      </a>
+      <div>
 
-      <a
-        href="/"
-        src={ exploreIcon }
-        data-testid="explore-bottom-btn"
-      >
-        <img src={ exploreIcon } alt="Explore" />
-      </a>
+        <a
+          href="/"
+          src={ drinkIcon }
+          data-testid="drinks-bottom-btn"
+        >
+          <img src={ drinkIcon } alt="Drinks" />
+        </a>
 
-      <a
-        href="/"
-        src={ mealIcon }
-        data-testid="food-bottom-btn"
-      >
-        <img src={ mealIcon } alt="Food" />
-      </a>
+        <a
+          href="/"
+          src={ exploreIcon }
+          data-testid="explore-bottom-btn"
+        >
+          <img src={ exploreIcon } alt="Explore" />
+        </a>
+
+        <a
+          href="/"
+          src={ mealIcon }
+          data-testid="food-bottom-btn"
+        >
+          <img src={ mealIcon } alt="Food" />
+        </a>
+
+      </div>
     </footer>
   );
 }
 
 export default Footer;
+
+// - Não tem footer na tela de login
+// - Tem footer na tela de principal de receitas de comidas [OK]
+// - Tem footer na tela de principal de receitas de bebidas [OK]
+// - Não tem footer na tela de detalhes de uma receita de comida
+// - Não tem footer na tela de detalhes de uma receita de bebida
+// - Não tem footer na tela de receita em processo de comida
+// - Não tem footer na tela de receita em processo de bebida
+// - Tem footer na tela de explorar [OK]
+// - Tem footer na tela de explorar comidas
+// - Tem footer na tela de explorar bebidas
+// - Tem footer na tela de explorar comidas por ingrediente
+// - Tem footer na tela de explorar bebidas por ingrediente
+// - Tem footer na tela de explorar comidas por local de origem
+// - Tem footer na tela de perfil [OK]
+// - Não tem footer na tela de receitas feitas
+// - Não tem footer na tela de receitas favoritas
--- src/pages/Bebidas.jsx
+++ src/pages/Bebidas.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 import useCurrentPage from '../context/hooks/useCurrentPage';
 
@@ -8,6 +9,7 @@ function Bebidas() {
   return (
     <div className="page">
       <Header showSearch />
+      <Footer />
     </div>
   );
 }
