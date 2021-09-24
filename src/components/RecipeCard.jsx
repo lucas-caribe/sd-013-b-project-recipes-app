45679335c805e8207cca8efcf941ad014388a038
Aleilton Chavenco <aleilton@gmail.com>
2021-09-24 20:23:29 -0300

    Requisito 57

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import shareIcon from '../images/shareIcon.svg';
 
+const INTERVAL = 3000;
+const copy = require('clipboard-copy');
+
 function RecipeCard({ recipe, index }) {
+  const [isCopyClipboard, setIsCopyClipboard] = useState(false);
   const {
     id, type, alcoholicOrNot, area, category, name, image, doneDate, tags,
   } = recipe;
+
+  useEffect(() => {
+    // chamar a função de cronometro
+    if (isCopyClipboard) {
+      const intervalId = setInterval(() => { setIsCopyClipboard(false); }, INTERVAL);
+      return () => { clearInterval(intervalId); };
+    }
+  }, [isCopyClipboard]);
+
+  // https://www.devmedia.com.br/javascript-get-url/28531
+  const url = `${window.location.protocol}//${window.location.host}`;
+
+  function getCopyClipboard() {
+    setIsCopyClipboard(true);
+    return (
+      copy(type === 'comida'
+        ? `${url}/comidas/${id}` : `${url}/bebidas/${id}`)
+    );
+  }
+
   return (
     <div className="recipes-done-card">
       <Link to={ type === 'comida' ? `/comidas/${id}` : `/bebidas/${id}` }>
@@ -34,12 +58,19 @@ function RecipeCard({ recipe, index }) {
             { doneDate }
           </span>
         </p>
-        <img
-          className="recipes-done-card-share"
-          data-testid={ `${index}-horizontal-share-btn` }
-          src={ shareIcon }
-          alt="Compartilhar"
-        />
+        <button
+          type="button"
+          onClick={
+            () => getCopyClipboard()
+          }
+        >
+          <img
+            className="recipes-done-card-share"
+            data-testid={ `${index}-horizontal-share-btn` }
+            src={ shareIcon }
+            alt="Compartilhar"
+          />
+        </button>
         { type === 'comida' && (
           <div className="recipes-done-tags">
             {tags.map((tag) => (
@@ -52,6 +83,7 @@ function RecipeCard({ recipe, index }) {
             ))}
           </div>) }
       </div>
+      { isCopyClipboard && <div className="alert-mensage">Link copiado!</div> }
     </div>
   );
 }
