17c79f19b585930b90939a737dc416f25c9505c9
Guilherme <guilhermerg1234@gmail.com>
2021-09-24 17:22:56 -0300

    83 e 84

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -2,9 +2,38 @@ import React from 'react';
 import Header from '../components/header';
 
 export default function Perfil() {
+  const { email } = JSON.parse(localStorage.getItem('user'));
+
   return (
     <div>
       <Header titlePage="Perfil" />
+      <p data-testid="profile-email">
+        {`Email: ${email}`}
+      </p>
+      {' '}
+      <br />
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+      >
+        Receitas Feitas
+      </button>
+      {' '}
+      <br />
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+      >
+        Receitas Favoritas
+      </button>
+      {' '}
+      <br />
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+      >
+        Sair
+      </button>
     </div>
   );
 }
