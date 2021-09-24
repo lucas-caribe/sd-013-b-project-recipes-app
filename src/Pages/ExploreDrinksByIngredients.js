c46f235799e8fe74222a8e510d60a74b853cd937
José Demeneghi <josedemeneghi_cba@hotmail.com>
2021-09-24 14:17:57 -0400

    requisitos 75 e 76

--- src/Pages/ExploreDrinksByIngredients.js
+++ src/Pages/ExploreDrinksByIngredients.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Footer from '../Components/Footer';
 import ProfileAvatar from '../Components/ProfileAvatar';
 
 function ExploreDrinksByIngredients() {
+  const [ingredients, setIngredients] = useState([]);
+
+  useEffect(() => {
+    async function getIngredients() {
+      const results = await fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list');
+      const { drinks } = await results.json();
+      let newArrayCat = [];
+      drinks.forEach((element, index) => {
+        const numberLimit = 12;
+        if (index < numberLimit) {
+          newArrayCat = [...newArrayCat, element.strIngredient1];
+        }
+      });
+      setIngredients(newArrayCat);
+    }
+    getIngredients();
+  }, []);
+  console.log(ingredients);
   return (
     <div>
       <h1 data-testid="page-title">Explorar Ingredientes</h1>
       <ProfileAvatar />
+      <main>
+        {
+          ingredients.map((item, index) => (
+            <div key={ item } data-testid={ `${index}-ingredient-card` }>
+              <img
+                src={ `https://www.thecocktaildb.com/images/ingredients/${item}-Small.png` }
+                alt={ item }
+                data-testid={ `${index}-card-img` }
+                // role="presentation"
+                // onClick={ () => history.push(`/comidas/${item.id}`) }
+                // width="50%"
+              />
+              <p data-testid={ `${index}-card-name` }>{item}</p>
+            </div>
+          ))
+        }
+      </main>
       <Footer />
     </div>
   );
--- src/Pages/ExploreFoodsByIngredients.js
+++ src/Pages/ExploreFoodsByIngredients.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Footer from '../Components/Footer';
 import ProfileAvatar from '../Components/ProfileAvatar';
 
 function ExploreFoodsByIngredients() {
+  const [ingredients, setIngredients] = useState([]);
+
+  useEffect(() => {
+    async function getIngredients() {
+      const results = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?i=list');
+      const { meals } = await results.json();
+      let arrayIngredients = [];
+      meals.forEach((element, index) => {
+        const numberLimit = 12;
+        if (index < numberLimit) {
+          arrayIngredients = [...arrayIngredients, element];
+        }
+      });
+      setIngredients(arrayIngredients);
+    }
+    getIngredients();
+  }, []);
+
   return (
     <div>
       <h1 data-testid="page-title">Explorar Ingredientes</h1>
       <ProfileAvatar />
+      <main>
+        {
+          ingredients.map((item, index) => (
+            <div key={ item.idIngredient } data-testid={ `${index}-ingredient-card` }>
+              <img
+                src={ `https://www.themealdb.com/images/ingredients/${item.strIngredient}-Small.png` }
+                alt={ item.strIngredient }
+                data-testid={ `${index}-card-img` }
+                // role="presentation"
+                // onClick={ () => history.push(`/comidas/${item.id}`) }
+                // width="50%"
+              />
+              <p data-testid={ `${index}-card-name` }>{item.strIngredient}</p>
+            </div>
+          ))
+        }
+      </main>
       <Footer />
     </div>
   );
