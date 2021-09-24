f75d7e6c973855914a0e83b54a1bdfb7033e5027
Natalia Camarinha <natcamarinha@gmail.com>
2021-09-24 19:39:35 -0300

    requisitos 28 e 31 da tela principal de receitas

--- src/pages/Bebidas.js
+++ src/pages/Bebidas.js
@@ -6,24 +6,33 @@ export default function Bebidas() {
   const [drinks, setDrinks] = useState([]);
   const [categories, setCategories] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState(undefined);
+  const [filterCategory, setFilterCategory] = useState([]);
 
-  const elementsNumber = 12;
+  const recipeNumber = 12;
 
   const categoryNumber = 5;
 
-  const pageTitle = 'Bebidas';
+  const pageTitle = 'Comidas';
 
   useEffect(() => {
     fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=')
-      .then((res) => res.json())
+      .then((response) => response.json())
       .then((data) => setDrinks(data.drinks));
   }, []);
 
   useEffect(() => {
     fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list')
-      .then((res) => res.json())
+      .then((response) => response.json())
       .then((data) => setCategories(data.drinks));
   }, []);
+
+  useEffect(() => {
+    if (selectedCategory !== undefined) {
+      fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${selectedCategory}`)
+        .then((response) => response.json())
+        .then((data) => setFilterCategory(data.drinks));
+    }
+  }, [selectedCategory]);
 
   function handleClick(category) {
     setSelectedCategory(category);
@@ -47,38 +56,40 @@ export default function Bebidas() {
               key={ index }
               onClick={ () => handleClick(category.strCategory) }
             >
-              { category.strCategory }
+              {category.strCategory}
             </button>
           )).slice(0, categoryNumber)}
       </div>
 
-      { drinks
-        .filter((drink) => drink.strCategory === selectedCategory)
-        .map((drinkSelected, index) => (
-          <div key={ index } data-testid={ `${index}-recipe-card` }>
-            <img
-              src={ drinkSelected.strDrinkThumb }
-              alt="drink"
-              width="100px"
-              data-testid={ `${index}-card-img` }
-            />
-            <p data-testid={ `${index}-card-name` }>{ drinkSelected.strDrink }</p>
-            <p data-testid={ `${selectedCategory}-category-filter` } />
-          </div>
-        )).slice(0, elementsNumber)}
+      <div>
+        { selectedCategory !== undefined ? (
+          filterCategory
+            .map((drinkFiltered, index) => (
+              <div key={ index } data-testid={ `${index}-recipe-card` }>
+                <img
+                  src={ drinkFiltered.strDrinkThumb }
+                  alt="meal"
+                  width="100px"
+                  data-testid={ `${index}-card-img` }
+                />
+                <p data-testid={ `${index}-card-name` }>{drinkFiltered.strDrink}</p>
+                <p data-testid={ `${selectedCategory}-category-filter` } />
+              </div>
+            )).slice(0, recipeNumber))
 
-      { drinks
-        .map((drink, index) => (
-          <div key={ index } data-testid={ `${index}-recipe-card` }>
-            <img
-              src={ drink.strDrinkThumb }
-              alt="drink"
-              width="100px"
-              data-testid={ `${index}-card-img` }
-            />
-            <p data-testid={ `${index}-card-name` }>{ drink.strDrink }</p>
-          </div>
-        )).slice(0, elementsNumber)}
+          : drinks
+            .map((drink, index) => (
+              <div key={ index } data-testid={ `${index}-recipe-card` }>
+                <img
+                  src={ drink.strDrinkThumb }
+                  alt="meal"
+                  width="100px"
+                  data-testid={ `${index}-card-img` }
+                />
+                <p data-testid={ `${index}-card-name` }>{drink.strDrink}</p>
+              </div>
+            )).slice(0, recipeNumber)}
+      </div>
       <Footer />
     </div>
   );
--- src/pages/Comidas.js
+++ src/pages/Comidas.js
@@ -6,8 +6,9 @@ export default function Comidas() {
   const [meals, setMeals] = useState([]);
   const [categories, setCategories] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState(undefined);
+  const [filterCategory, setFilterCategory] = useState([]);
 
-  const elementsNumber = 12;
+  const recipeNumber = 12;
 
   const categoryNumber = 5;
 
@@ -15,32 +16,35 @@ export default function Comidas() {
 
   useEffect(() => {
     fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=')
-      .then((res) => res.json())
+      .then((response) => response.json())
       .then((data) => setMeals(data.meals));
   }, []);
 
   useEffect(() => {
     fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list')
-      .then((res) => res.json())
+      .then((response) => response.json())
       .then((data) => setCategories(data.meals));
   }, []);
+
+  useEffect(() => {
+    if (selectedCategory !== undefined) {
+      fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${selectedCategory}`)
+        .then((response) => response.json())
+        .then((data) => setFilterCategory(data.meals));
+    }
+  }, [selectedCategory]);
 
   function handleClick(category) {
     setSelectedCategory(category);
   }
 
-  if (meals.length < 1) {
+  if (meals.length === 0) {
     return <h6>Loading...</h6>;
   }
-
-  console.log(meals);
-  console.log(categories);
-  console.log(selectedCategory);
 
   return (
     <div>
       <Header value={ pageTitle } />
-<<<<<<< HEAD
       <div>
         <button type="button" onClick={ () => handleClick(undefined) }>All</button>
 
@@ -52,29 +56,27 @@ export default function Comidas() {
               key={ index }
               onClick={ () => handleClick(category.strCategory) }
             >
-              { category.strCategory }
+              {category.strCategory}
             </button>
           )).slice(0, categoryNumber)}
       </div>
 
       <div>
         { selectedCategory !== undefined ? (
-          meals
-            .filter((meal) => meal.strCategory === selectedCategory)
-            .map((mealSelected, index) => (
+          filterCategory
+            .map((mealFiltered, index) => (
               <div key={ index } data-testid={ `${index}-recipe-card` }>
-                { console.log(mealSelected) }
                 <img
-                  src={ mealSelected.strMealThumb }
+                  src={ mealFiltered.strMealThumb }
                   alt="meal"
                   width="100px"
                   data-testid={ `${index}-card-img` }
                 />
-                <p data-testid={ `${index}-card-name` }>{ mealSelected.strMeal }</p>
+                <p data-testid={ `${index}-card-name` }>{mealFiltered.strMeal}</p>
                 <p data-testid={ `${selectedCategory}-category-filter` } />
               </div>
-            )).slice(0, elementsNumber)
-        )
+            )).slice(0, recipeNumber))
+
           : meals
             .map((meal, index) => (
               <div key={ index } data-testid={ `${index}-recipe-card` }>
@@ -84,25 +86,11 @@ export default function Comidas() {
                   width="100px"
                   data-testid={ `${index}-card-img` }
                 />
-                <p data-testid={ `${index}-card-name` }>{ meal.strMeal }</p>
+                <p data-testid={ `${index}-card-name` }>{meal.strMeal}</p>
               </div>
-            )).slice(0, elementsNumber)}
+            )).slice(0, recipeNumber)}
       </div>
-=======
-      { meals
-        .map((meal, index) => (
-          <div key={ index } data-testid={ `${index}-recipe-card` }>
-            <img
-              src={ meal.strMealThumb }
-              alt="drink"
-              width="100px"
-              data-testid={ `${index}-card-img` }
-            />
-            <p data-testid={ `${index}-card-name` }>{ meal.strMeal }</p>
-          </div>
-        )).slice(0, elementsNumber)}
       <Footer />
->>>>>>> da7e1b0e577e53f8cee65c865c7fb8d538451566
     </div>
   );
 }
