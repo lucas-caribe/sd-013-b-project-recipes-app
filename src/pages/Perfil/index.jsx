71dd56915086c03c3241477b756f1ce1a16f582d
alanmdf <alanmoreirascr@gmail.com>
2021-09-24 09:51:29 -0300

    Conflitos nos testes resolvidos

--- src/pages/Perfil/index.jsx
+++ src/pages/Perfil/index.jsx
@@ -2,20 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
+import { useAuth } from '../../context';
 
 function Perfil({ history }) {
   const name = JSON.parse(localStorage.getItem('user'));
+  const { email } = useAuth();
 
   function clearAndRedirect() {
     localStorage.clear();
     history.push('/');
   }
+
+  const renderEmail = () => {
+    if (name) {
+      return <h1 data-testid="profile-email">{name.email}</h1>;
+    } return <h1 data-testid="profile-email">{email}</h1>;
+  };
+
   return (
     <main>
       <Header pageTitle="Perfil" showSearchIcon={ false } />
-      <Footer />
       <div>
-        <h1 data-testid="profile-email">{name.email}</h1>
+        {renderEmail()}
         <button
           type="button"
           data-testid="profile-done-btn"
@@ -38,6 +46,7 @@ function Perfil({ history }) {
           Sair
         </button>
       </div>
+      <Footer />
     </main>
   );
 }
