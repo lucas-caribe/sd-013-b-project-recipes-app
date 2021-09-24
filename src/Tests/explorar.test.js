ceb17bdb27f677907cc30cd94550b08dbb8c8844
Raphael Rodrigues <raphael_rpc@outlook.com>
2021-09-24 15:56:32 -0300

    mod nos test e add serv fetchRandom

--- src/Tests/explorar.test.js
+++ src/Tests/explorar.test.js
@@ -1,5 +1,5 @@
 import { screen } from '@testing-library/dom';
-import userEvent from '@testing-library/user-event';
+// import userEvent from '@testing-library/user-event';
 import React from 'react';
 import App from '../App';
 import renderWithRouterAndRedux from './helper/RenderWithRouterAndRedux';
@@ -9,11 +9,11 @@ describe('Testando o componente Explorar ', () => {
     const { history } = renderWithRouterAndRedux(<App />);
     history.push('/explorar');
     const NUMBER_BUTTONS = 2;
-    const buttons = screen.getAllByRole("button");
-    expect(buttons.length).toBe(NUMBER_BUTTONS)
+    const buttons = screen.getAllByRole('button');
+    expect(buttons.length).toBe(NUMBER_BUTTONS);
     // const button1 = screen.getByTestId('explore-food')
     // const button2 = screen.getByTestId('explore-drinks')
     // expect(button1).toBeInTheDocument();
     // expect(button2).toBeInTheDocument();
-  })
+  });
 });
