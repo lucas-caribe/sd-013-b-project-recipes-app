aed815c03994e4519ade4a74986c19b8fc735799
Lucas <lucascardim777@gmail.com>
2021-09-24 17:46:24 -0300

    Glass style for login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import '../components/css/LoginStylesheet.css';
 
 export default function Login({ history }) {
   const [email, setEmail] = useState('');
@@ -18,40 +19,54 @@ export default function Login({ history }) {
     const user = { email };
 
     localStorage.setItem('mealsToken', JSON.stringify(tokens.mealsToken));
-    localStorage.setItem('cocktailsToken', JSON.stringify(tokens.cocktailsToken));
+    localStorage.setItem(
+      'cocktailsToken',
+      JSON.stringify(tokens.cocktailsToken)
+    );
     localStorage.setItem('user', JSON.stringify(user));
 
     history.push('/comidas');
   };
 
   return (
-    <div>
-      <input
-        required
-        data-testid="email-input"
-        type="text"
-        name="email"
-        onChange={ handleChange }
-        value={ email }
-        placeholder="e-mail"
-      />
-      <input
-        required
-        data-testid="password-input"
-        type="password"
-        name="password"
-        onChange={ handleChange }
-        placeholder="senha"
-      />
-      <button
-        data-testid="login-submit-btn"
-        type="submit"
-        value={ password }
-        disabled={ !(emailValidator.test(email) && password.length > minLenghtPassword) }
-        onClick={ handleClick }
-      >
-        Entrar
-      </button>
+    <div className="login">
+      <h1>Login</h1>
+      <formfield className="form">
+        <input
+          className="field"
+          required
+          data-testid="email-input"
+          type="text"
+          name="email"
+          onChange={ handleChange }
+          value={ email }
+          placeholder="e-mail"
+        />
+        <input
+          className="field"
+          required
+          data-testid="password-input"
+          type="password"
+          name="password"
+          onChange={ handleChange }
+          placeholder="senha"
+        />
+        <div className="button-div">
+
+          <button
+            className="button"
+            data-testid="login-submit-btn"
+            type="submit"
+            value={ password }
+            disabled={
+              !(emailValidator.test(email) && password.length > minLenghtPassword)
+            }
+            onClick={ handleClick }
+          >
+            Entrar
+          </button>
+        </div>
+      </formfield>
     </div>
   );
 }
