25cf6d16e9c2f90add7169e4bf1f83e6fba544b5
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2021-09-27 19:21:38 -0300

    Perfil page

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -1,15 +1,43 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
-const Perfil = () => (
-  <>
-    <Header title="Perfil" displaySearchBtn={ false } />
-    <div>
-      Sou a página de perfil
-    </div>
-    <Footer />
-  </>
-);
+const Perfil = () => {
+  const history = useHistory();
+  // substito para mapStateToProps
+  const email = useSelector(({ user }) => user.email);
+  return (
+    <>
+      <Header title="Perfil" displaySearchBtn={ false } />
+      <div>
+        <p data-testid="profile-email">{ email }</p>
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+          onClick={ () => history.push('/receitas-feitas') }
+        >
+          Receitas Feitas
+        </button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+          onClick={ () => history.push('/receitas-favoritas') }
+        >
+          Receitas Favoritas
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ () => history.push('/') }
+        >
+          Sair
+        </button>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
 export default Perfil;
