44cc4b2c5912c25ef9a633ace9b74bd58ce530f0
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2021-09-27 19:12:08 -0300

    Perfil page

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,8 +1,14 @@
 export const allActions = {
   ADD_FOODS: 'ADD_FOODS',
+  SAVE_EMAIL: 'SAVE_EMAIL',
 };
 
 export const saveFoods = (foods) => ({
   type: allActions.ADD_FOODS,
   payload: foods,
+});
+
+export const saveEmail = (email) => ({
+  type: allActions.SAVE_EMAIL,
+  payload: email,
 });
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import foodReducer from './foodReducer';
+import userReducer from './userReducer';
 
 const rootReducer = combineReducers({
   foods: foodReducer,
+  user: userReducer,
 });
 
 export default rootReducer;
