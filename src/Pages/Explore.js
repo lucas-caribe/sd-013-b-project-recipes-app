61123fd66915674540a969dd16e053d5bfc3592e
Gabriel <gribeiro237@gmail.com>
2021-09-27 18:03:47 -0300

    requisitos 67-74 revisado

--- src/Pages/Explore.js
+++ src/Pages/Explore.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import Explorer from '../Components/Explorer';
 import ExploreBy from '../Components/ExplorerBy';
 import Header from '../Components/Header';
 import LowerMenu from '../Components/LowerMenu';
@@ -24,7 +23,6 @@ const Explore = () => {
         right={ filter === 'area' ? 'search' : 'none' }
       />
       {main}
-      <Explorer />
       <ExploreBy />
       <LowerMenu />
     </div>
