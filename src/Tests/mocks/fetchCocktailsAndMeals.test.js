afd0f8d24dfde2b9483c49fd6ef8435af100fe2d
Joao Victor <joaogmavictor@gmail.com>
2021-09-28 18:33:08 -0300

    Arrumando mock do Fetch

--- src/Tests/mocks/fetchCocktailsAndMeals.test.js
+++ src/Tests/mocks/fetchCocktailsAndMeals.test.js
@@ -3,13 +3,19 @@ import { fetchCocktailArray, fetchMealsArray } from '../../services/fetchItens';
 import App from '../../App';
 
 describe('Testa as funções fetchCocktailArray && fetchMealsArray', () => {
-  // global.fetch = jest.fn(async () => ({ json: async () => ({ drinks: [] }) }));
+  global.fetch = jest.fn(async () => ({ ok: true, json: async () => ({ drinks: [] }) }));
 
-  fetchCocktailArray = jest.fn(() => ({ drinks: [] }));
+  // fetchCocktailArray = jest.fn(() => ({ drinks: [] }));
 
-  test('Testa a Função fetchCocktailArray', () => {
-    fetchCocktailArray().then((r) => {
-      console.log(r);
-    });
+  test('Testa a Função fetchCocktailArray', async () => {
+    try {
+      const teste = await fetchCocktailArray();
+      expect(teste).toEqual({ drinks: [] });
+    } catch (error) {
+      console.log(error);
+    }
+    // fetchCocktailArray().then((r) => {
+    //   console.log(r);
+    // });
   });
 });
