5d0308034094e09626e9a114e7b7ee8a4077a4ac
Guilherme da Costa <guilhermedc93@gmail.com>
2021-09-28 14:54:43 -0300

    done recipes conpleted

--- src/components/DoneCard.jsx
+++ src/components/DoneCard.jsx
@@ -1,33 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import shareIcon from '../images/shareIcon.svg';
 
-function DoneCard() {
-  let doneRecipesStorage = localStorage.getItem('doneRecipes');
-  doneRecipesStorage = JSON.parse(doneRecipesStorage);
-  console.log(doneRecipesStorage);
+const copy = require('clipboard-copy');
 
-  if (!doneRecipesStorage) {
-    console.log('ok');
-  }
+function DoneCard({ filteredRecipes }) {
+  const [modal, setModal] = useState(false);
+
+  const handleShareButton = ({ id, type }) => {
+    const modalTime = 2000;
+    setModal(true);
+    setTimeout(() => setModal(false), modalTime);
+    if (type === 'comida') return copy(`http://localhost:3000/comidas/${id}`);
+    copy(`http://localhost:3000/bebidas/${id}`);
+  };
+
+  const getHorizontalTopText = (recipe) => {
+    if (recipe.type === 'bebida') return recipe.alcoholicOrNot;
+    return `${recipe.area} - ${recipe.category}`;
+  };
 
   return (
     <div>
-      cards
+      { modal && <span>Link copiado!</span> }
+      { filteredRecipes.map((recipe, index) => (
+        <div key={ recipe.id }>
+          <Link to={ `/${recipe.type}s/${recipe.id}` }>
+            <img
+              data-testid={ `${index}-horizontal-image` }
+              src={ recipe.image }
+              alt="imagem da receita"
+              style={ { height: '150px' } }
+            />
+            <p data-testid={ `${index}-horizontal-name` }>{ recipe.name }</p>
+          </Link>
+          <p
+            data-testid={ `${index}-horizontal-top-text` }
+          >
+            { getHorizontalTopText(recipe) }
+          </p>
+          <p data-testid={ `${index}-horizontal-done-date` }>{ recipe.doneDate }</p>
+          <button
+            type="button"
+            onClick={ () => handleShareButton(recipe) }
+          >
+            <img
+              src={ shareIcon }
+              alt="botão para compartilhar"
+              data-testid={ `${index}-horizontal-share-btn` }
+            />
+          </button>
+          <div>
+            {recipe.tags.map((tag) => (
+              <span
+                data-testid={ `${index}-${tag}-horizontal-tag` }
+                key={ tag }
+              >
+                { tag }
+              </span>
+            ))}
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
 
 export default DoneCard;
 
-/*
-[{
-    id: id-da-receita,
-    type: comida-ou-bebida,
-    area: area-da-receita-ou-texto-vazio,
-    category: categoria-da-receita-ou-texto-vazio,
-    alcoholicOrNot: alcoholic-ou-non-alcoholic-ou-texto-vazio,
-    name: nome-da-receita,
-    image: imagem-da-receita,
-    doneDate: quando-a-receita-foi-concluida,
-    tags: array-de-tags-da-receita-ou-array-vazio
-}]
-*/
+DoneCard.propTypes = {
+  filteredRecipes: PropTypes.arrayOf(PropTypes.any),
+}.isRequired;
--- src/pages/ReceitasFeitas.jsx
+++ src/pages/ReceitasFeitas.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DoneRecipesFilter from '../components/DoneRecipesFilters';
 import Header from '../components/Header';
 import DoneCard from '../components/DoneCard';
 
 function ReceitasFeitas() {
+  const [actualFilter, setActualFilter] = useState(false);
+  let doneRecipesStorage = localStorage.getItem('doneRecipes');
+  doneRecipesStorage = JSON.parse(doneRecipesStorage);
+
+  const handleFilter = (filterFactor) => {
+    if (!filterFactor) return doneRecipesStorage;
+    const filteredRecipes = doneRecipesStorage.filter(({ type }) => (
+      type === filterFactor
+    ));
+    return filteredRecipes;
+  };
+
   return (
     <div>
       <Header pageTitle="Receitas Feitas" haveHeader={ false } />
-      <DoneRecipesFilter />
-      <DoneCard />
+      <DoneRecipesFilter setActualFilter={ setActualFilter } />
+      { doneRecipesStorage && <DoneCard
+        filteredRecipes={ handleFilter(actualFilter) }
+      /> }
     </div>
   );
 }
