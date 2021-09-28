0e97fee3e2a7e1789ea2b6511c5ab0da10e6c42f
Mariana Garcia <maridgarcia045@gmail.com>
2021-09-28 18:59:55 -0300

    Finaliza requisito 74

--- src/pages/ExplorarBebidas.jsx
+++ src/pages/ExplorarBebidas.jsx
@@ -1,10 +1,27 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import useCurrentPage from '../context/hooks/useCurrentPage';
 
 function ExplorarBebidas() {
   useCurrentPage('Explorar Bebidas');
+
+  const [drink, setDrink] = useState({});
+  const history = useHistory();
+
+  useEffect(() => {
+    const fetchRandomDrink = async () => {
+      const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php');
+      const data = await response.json();
+      setDrink(data.drinks[0].idDrink);
+    };
+    fetchRandomDrink();
+  }, []);
+
+  function handleClick() {
+    history.push(`/bebidas/${drink}`);
+  }
+
   return (
     <div>
       <Link to="/explorar/bebidas/ingredientes">
@@ -18,6 +35,7 @@ function ExplorarBebidas() {
       <button
         type="button"
         data-testid="explore-surprise"
+        onClick={ handleClick }
       >
         Me Surpreenda!
       </button>
--- src/pages/ExplorarComidas.jsx
+++ src/pages/ExplorarComidas.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
 import useCurrentPage from '../context/hooks/useCurrentPage';
 
 function ExplorarComidas() {
   useCurrentPage('Explorar Comidas');
+  const [food, setFood] = useState({});
+  const history = useHistory();
+
+  useEffect(() => {
+    const fetchRandomFood = async () => {
+      const response = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+      const data = await response.json();
+      setFood(data.meals[0].idMeal);
+    };
+    fetchRandomFood();
+  }, []);
+
+  const handleClick = () => {
+    history.push(`/comidas/${food}`);
+  };
+
   return (
     <div>
       <Link to="/explorar/comidas/ingredientes">
@@ -26,6 +42,7 @@ function ExplorarComidas() {
       <button
         type="button"
         data-testid="explore-surprise"
+        onClick={ handleClick }
       >
         Me Surpreenda!
       </button>
