efb5b7947552c311b8c997d5c6b9c6f39293ff96
AlefSloan <83962082+AlefSloan@users.noreply.github.com>
2021-09-28 14:57:34 -0300

    ShareBar error solved

--- src/component/SearchBar.js
+++ src/component/SearchBar.js
@@ -1,32 +1,7 @@
-import React, { useEffect, useContext } from 'react';
-import { useHistory } from 'react-router';
+import React from 'react';
 import Context from '../context/Context';
 
 export default function SearchBar() {
-  const history = useHistory();
-  const { filterRadio,
-    setFilterRadio,
-    filterText,
-    setFilterText,
-    fetchFood,
-    fetchDrink,
-    filteredItem,
-  } = useContext(Context);
-
-  useEffect(() => {
-    if (filterRadio === 'f' && filterText.length > 1) {
-      global.alert('Sua busca deve conter somente 1 (um) caracter');
-    }
-  }, [filterText]);
-
-  useEffect(() => {
-    if (filteredItem.length === 1 && history.location.pathname === '/comidas') {
-      history.push(`/comidas/${filteredItem[0].idMeal}`);
-    } if (filteredItem.length === 1 && history.location.pathname === '/bebidas') {
-      history.push(`/bebidas/${filteredItem[0].idDrink}`);
-    }
-  }, [filteredItem]);
-
   return (
     <div>
       <input data-testid="search-input" type="text" placeholder="DIGITE SUA COMIDA" />
