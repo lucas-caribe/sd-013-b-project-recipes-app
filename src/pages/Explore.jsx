488edd5d48585fcd403344ad08d2322bb26c0d3f
José Breno <brenojbs@hotmail.com>
2021-09-28 18:42:41 -0300

    Requisito 67, 68, 69 pronto!

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 
 export default function Explore() {
   return (
     <div>
-      <h2>Explore</h2>
       <Header />
+      <div>
+        <Link to="/explorar/comidas">
+          <button type="button" data-testid="explore-food">
+            Explorar Comidas
+          </button>
+        </Link>
+      </div>
+      <div>
+        <Link to="/explorar/bebidas">
+          <button type="button" data-testid="explore-drinks">
+            Explorar Bebidas
+          </button>
+        </Link>
+      </div>
+      Footer aqui!!!
     </div>
   );
 }
