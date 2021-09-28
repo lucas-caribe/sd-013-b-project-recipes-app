4236a7ee7a57ff270fc648d8a0fc42e457c78bcb
José Breno <brenojbs@hotmail.com>
2021-09-28 14:36:21 -0300

    Teste requisito 67, 68, 69

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 
 export default function Explore() {
   return (
     <div>
-      <h2>Explore</h2>
       <Header />
+      <div>
+        <button type="button" data-testid="explore-food">
+          <Link to="/explorar/comidas">
+            Explore Comidas
+          </Link>
+        </button>
+      </div>
+      <div>
+        <button type="button" data-testid="explore-drinks">
+          <Link to="/explorar/bebidas">
+            Explore Bebidas
+          </Link>
+        </button>
+      </div>
+      Footer aqui!!!
     </div>
   );
 }
