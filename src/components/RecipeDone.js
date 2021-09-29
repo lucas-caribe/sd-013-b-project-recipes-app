baf1ae0732e0099a027e56c327af06ae9496b27f
VitorDiamantino <vitor.diamantino8@gmail.com>
2021-09-29 20:00:49 -0300

    Requisito 54

--- src/components/RecipeDone.js
+++ src/components/RecipeDone.js
@@ -1,28 +1,100 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function RecipeDone() {
+  const [recipeDone] = useState([0]);
+
   return (
-    <div>
-      <div>
-        <button
-          data-testid="filter-by-all-btn"
-          type="button"
-        >
-          All
-        </button>
-        <button
-          data-testid="filter-by-food-btn"
-          type="button"
-        >
-          Food
-        </button>
-        <button
-          data-testid="filter-by-drink-btn"
-          type="button"
-        >
-          Drinks
-        </button>
-      </div>
+    <div className="recipe-done">
+      <h1>Receitas Prontas</h1>
+      <button
+        data-testid="filter-by-all-btn"
+        type="button"
+      >
+        All
+      </button>
+      <button
+        data-testid="filter-by-food-btn"
+        type="button"
+      >
+        Food
+      </button>
+      <button
+        data-testid="filter-by-drink-btn"
+        type="button"
+      >
+        Drinks
+      </button>
+
+      <p>
+        {
+          recipeDone.map((value, index) => (
+            <p
+              key={ index }
+              data-testid={ `${index}-horizontal-image` }
+            >
+              { value }
+            </p>
+          ))
+        }
+      </p>
+
+      <p>
+        {
+          recipeDone.map((value, index) => (
+            <p
+              key={ index }
+              data-testid={ `${index}-horizontal-top-text` }
+            >
+              { value }
+            </p>
+          ))
+        }
+      </p>
+
+      <p>
+        {
+          recipeDone.map((value, index) => (
+            <p
+              key={ index }
+              data-testid={ `${index}-horizontal-name` }
+            >
+              { value }
+            </p>
+          ))
+        }
+      </p>
+      <p>
+        {
+          recipeDone.map((value, index) => (
+            <p
+              key={ index }
+              data-testid={ `${index}-horizontal-done-date` }
+            >
+              { value }
+            </p>
+          ))
+        }
+      </p>
+      <p>
+        {
+          recipeDone.map((value, index) => (
+            <p
+              key={ index }
+              data-testid={ `${index}-horizontal-share-btn` }
+            >
+              { value }
+            </p>
+          ))
+        }
+      </p>
+      <div data-testid="0-Pasta-horizontal-tag" />
+      <div data-testid="0-Curry-horizontal-tag" />
+      <div data-testid="0-Pasta-horizontal-tag" />
+      <div data-testid="1-horizontal-image" />
+      <div data-testid="1-horizontal-top-text" />
+      <div data-testid="1-horizontal-name" />
+      <div data-testid="1-horizontal-share-btn" />
+      <div data-testid="1-horizontal-done-date" />
     </div>
   );
 }
