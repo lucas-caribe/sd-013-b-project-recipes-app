a679740c987de857c8a5ebed85f8eb49b7481fa4
Letícia Castro <aicitelks@gmail.com>
2021-09-30 20:19:21 -0300

    Correção linter

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -3,6 +3,30 @@ import PropTypes from 'prop-types';
 import Context from './Context';
 
 function Provider({ children }) {
+  // RECEITAS MOCKADAS PARA FINS DE TESTE EM "Receitas Feitas e Favoritas"
+  const RECEITAS_MOCK = [
+    {
+      idMeal: 52771,
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
+      strCategory: 'Vegetarian',
+      strMeal: 'Spicy Arrabiata Penne',
+      strTags: 'Pasta, Curry, Macarrão',
+      strArea: 'Italian',
+      date: '23/06/2020',
+      type: 'Meal',
+    },
+    {
+      idDrink: 178319,
+      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
+      strCategory: 'Ordinary Drink',
+      strAlcoholic: 'Alcoholic',
+      strDrink: 'Aquamarine',
+      strTags: null,
+      date: '23/06/2020',
+      type: 'Drink',
+    },
+  ];
+
   const [statusLoginBtn, setStatusLoginBtn] = useState(true);
   const [emailIsValid, setEmailIsValid] = useState(false);
   const [passwordIsValid, setPasswordIsValid] = useState(false);
@@ -10,56 +34,13 @@ function Provider({ children }) {
   const [login, setLogin] = useState('');
   const [showSearchBar, setShowSearchBar] = useState(false);
   const [allRecipes, setAllRecipes] = useState([]);
-  // ESSE ESTADO TEM QUE SER SETADO NO BOTÃO 'FINALIZAR RECEITA' DA 'RECEITA EM PROGRESSO'
-  const [allRecipesDone, setAllRecipesDone] = useState([
-    // OBJETOS MOCKADOS PARA FINS DE TESTE
-    {
-      idMeal: 52771,
-      strMealThumb: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
-      strCategory: 'Vegetarian',
-      strMeal: 'Spicy Arrabiata Penne',
-      strTags: 'Pasta, Curry, Macarrão',
-      strArea: 'Italian',
-      date: '23/06/2020',
-      type: 'Meal',
-    },
-    {
-      idDrink: 178319,
-      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
-      strCategory: 'Ordinary Drink',
-      strAlcoholic: 'Alcoholic',
-      strDrink: 'Aquamarine',
-      strTags: null,
-      date: '23/06/2020',
-      type: 'Drink',
-    },
-  ]);
-  const [filterRecipeDone, setFilterRecipeDone] = useState([]);
-  const [linkCopied, setLinkCopied] = useState(false);
 
-  const [favoritesRecipes, setFavoritesRecipes] = useState([
-    // OBJETOS MOCKADOS PARA FINS DE TESTE
-    {
-      idMeal: 52771,
-      strMealThumb: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
-      strCategory: 'Vegetarian',
-      strMeal: 'Spicy Arrabiata Penne',
-      strTags: 'Pasta, Curry, Macarrão',
-      strArea: 'Italian',
-      date: '23/06/2020',
-      type: 'Meal',
-    },
-    {
-      idDrink: 178319,
-      strDrinkThumb: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
-      strCategory: 'Ordinary Drink',
-      strAlcoholic: 'Alcoholic',
-      strDrink: 'Aquamarine',
-      strTags: null,
-      date: '23/06/2020',
-      type: 'Drink',
-    },
-  ]);
+  const [linkCopied, setLinkCopied] = useState(false);
+  // ESSE ESTADO TEM QUE SER SETADO NO BOTÃO 'FINALIZAR RECEITA' DA 'RECEITA EM PROGRESSO'
+  const [allRecipesDone, setAllRecipesDone] = useState(RECEITAS_MOCK);
+  const [filterRecipeDone, setFilterRecipeDone] = useState([]);
+
+  const [favoritesRecipes, setFavoritesRecipes] = useState(RECEITAS_MOCK);
   const [filterFavoritesRecipes, setFilterFavoritesRecipes] = useState([]);
 
   const [inputText, setInputText] = useState('');
