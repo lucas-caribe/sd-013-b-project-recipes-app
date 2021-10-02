cdf0de7e3d4e9233a37492e012f3e562bdd994d4
Lopes <gus.lcampos38@gmail.com>
2021-10-01 22:21:12 -0300

    finalize up to req 66

--- src/Pages/FavoriteRecipes.jsx
+++ src/Pages/FavoriteRecipes.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import copy from 'clipboard-copy';
+import { useHistory } from 'react-router';
 import Header from '../components/Header';
 import shareIcon from '../images/shareIcon.svg';
 import whiteHeartIcon from '../images/whiteHeartIcon.svg';
@@ -8,8 +9,12 @@ import handleLike from '../miscellaneous/misc3';
 
 export default function FavoriteRecipes() {
   const favRec = JSON.parse(localStorage.getItem('favoriteRecipes'));
+  // localStorage.setItem('favorite', JSON.stringify(favRec));
   const [local, setLocal] = useState();
   const [hidden, setHidden] = useState();
+  const [filter, setFilter] = useState('off');
+  const [renderFilter, setRenderFilter] = useState(false);
+  const history = useHistory();
 
   function srcSetter(obj) {
     if (!localStorage.getItem('favoriteRecipes')) return whiteHeartIcon;
@@ -29,15 +34,32 @@ export default function FavoriteRecipes() {
     setHidden(true);
   }
 
-  return (
-    <>
-      <Header title="Favorites" />
-      <div>
-        <button type="button" data-testid="filter-by-all-btn">All</button>
-        <button type="button" data-testid="filter-by-food-btn">Food</button>
-        <button type="button" data-testid="filter-by-drink-btn">Drinks</button>
+  function filterFavsFood(favs) {
+    localStorage.setItem('favorite', JSON
+      .stringify((favs.filter((fav) => fav.type === 'comida'))));
+    setFilter('on');
+    setRenderFilter(!renderFilter);
+  }
+
+  function filterFavsDrink(favs) {
+    localStorage.setItem('favorite', JSON
+      .stringify((favs.filter((fav) => fav.type === 'bebida'))));
+    setFilter('on');
+    setRenderFilter(!renderFilter);
+  }
+
+  function allFavs(favs) {
+    localStorage.setItem('favorite', JSON
+      .stringify(favs));
+    setFilter('off');
+    setRenderFilter(!renderFilter);
+  }
+
+  function renderFavs() {
+    if (filter === 'on') {
+      return (
         <div>
-          { favRec.map((rec, index) => {
+          { JSON.parse(localStorage.getItem('favorite')).map((rec, index) => {
             if (rec.type === 'comida') {
               return (
                 <div key={ index }>
@@ -46,8 +68,9 @@ export default function FavoriteRecipes() {
                   >
                     {`${rec.area} - ${rec.category}`}
                   </h3>
-                  <h2 data-testid={ `${index}-horizontal-name` }>{rec.name}</h2>
+                  <h2 onClick={ () => history.push(`/comidas/${rec.id}`) } data-testid={ `${index}-horizontal-name` }>{rec.name}</h2>
                   <img
+                    onClick={ () => history.push(`/comidas/${rec.id}`) }
                     src={ rec.image }
                     alt="rec"
                     data-testid={ `${index}-horizontal-image` }
@@ -76,11 +99,12 @@ export default function FavoriteRecipes() {
                 >
                   {`${rec.alcoholicOrNot} - ${rec.category}`}
                 </h3>
-                <h2 data-testid={ `${index}-horizontal-name` }>{rec.name}</h2>
+                <h2 onClick={ () => history.push(`/bebidas/${rec.id}`) } data-testid={ `${index}-horizontal-name` }>{rec.name}</h2>
                 <img
                   src={ rec.image }
                   alt="rec"
                   data-testid={ `${index}-horizontal-image` }
+                  onClick={ () => history.push(`/bebidas/${rec.id}`) }
                 />
                 <input
                   onClick={ () => handleShareBebida(rec.id) }
@@ -98,7 +122,83 @@ export default function FavoriteRecipes() {
                 />
               </div>);
           })}
-        </div>
+        </div>);
+    } return (
+      <div>
+        { favRec.map((rec, index) => {
+          if (rec.type === 'comida') {
+            return (
+              <div key={ index }>
+                <h3
+                  data-testid={ `${index}-horizontal-top-text` }
+                >
+                  {`${rec.area} - ${rec.category}`}
+                </h3>
+                <h2 onClick={ () => history.push(`/comidas/${rec.id}`) } data-testid={ `${index}-horizontal-name` }>{rec.name}</h2>
+                <img
+                  onClick={ () => history.push(`/comidas/${rec.id}`) }
+                  src={ rec.image }
+                  alt="rec"
+                  data-testid={ `${index}-horizontal-image` }
+                />
+                <input
+                  onClick={ () => handleShareComida(rec.id) }
+                  data-testid={ `${index}-horizontal-share-btn` }
+                  type="image"
+                  alt="share"
+                  src={ shareIcon }
+                />
+                <input
+                  data-testid={ `${index}-horizontal-favorite-btn` }
+                  type="image"
+                  alt="fav-btn"
+                  src={ srcSetter(rec) }
+                  onClick={ () => handleLike(rec, setLocal, local) }
+                />
+              </div>
+            );
+          }
+          return (
+            <div key={ index }>
+              <h3
+                data-testid={ `${index}-horizontal-top-text` }
+              >
+                {`${rec.alcoholicOrNot} - ${rec.category}`}
+              </h3>
+              <h2 onClick={ () => history.push(`/bebidas/${rec.id}`) } data-testid={ `${index}-horizontal-name` }>{rec.name}</h2>
+              <img
+                onClick={ () => history.push(`/bebidas/${rec.id}`) }
+                src={ rec.image }
+                alt="rec"
+                data-testid={ `${index}-horizontal-image` }
+              />
+              <input
+                onClick={ () => handleShareBebida(rec.id) }
+                data-testid={ `${index}-horizontal-share-btn` }
+                type="image"
+                alt="share"
+                src={ shareIcon }
+              />
+              <input
+                data-testid={ `${index}-horizontal-favorite-btn` }
+                type="image"
+                alt="fav-btn"
+                src={ srcSetter(rec) }
+                onClick={ () => handleLike(rec, setLocal, local) }
+              />
+            </div>);
+        })}
+      </div>);
+  }
+
+  return (
+    <>
+      <Header title="Favorites" />
+      <div>
+        <button type="button" onClick={ () => allFavs(favRec) } data-testid="filter-by-all-btn">All</button>
+        <button type="button" onClick={ () => filterFavsFood(favRec) } data-testid="filter-by-food-btn">Food</button>
+        <button type="button" onClick={ () => filterFavsDrink(favRec) } data-testid="filter-by-drink-btn">Drinks</button>
+        {renderFavs()}
         {hidden ? <div>Link copiado!</div> : null}
       </div>
     </>
